7ae978140ac2484ad415463730ba1fb45d4047fe
Torsten Bögershausen <torsten.bogershausen@esss.se>
2017-01-16 08:10:10 +0100

    Test 090: More tests
    Add more tests:
    - check the VAL field against the RBV field
    - Search for the limit switches.
      To get this right when the DIR fiels is true, use dial coordinates
      instead of user coordinates.
    - Ignore limit switch, when moving away from the limit switch
    
    Side note: the whole smoke test is still shaky. More work to be done.

--- test/nose_tests/090_Ethercat-SmokeTest-TWF_CNEN.py
+++ test/nose_tests/090_Ethercat-SmokeTest-TWF_CNEN.py
@@ -17,6 +17,7 @@ import time
 from motor_lib import motor_lib
 ###
 
+epics.ca.DEFAULT_CONNECTION_TIMEOUT=10.
 polltime = 0.1
 
 def calcAlmostEqual(motor, tc_no, expected, actual, maxdelta):
@@ -25,43 +26,43 @@ def calcAlmostEqual(motor, tc_no, expected, actual, maxdelta):
     print '%s: assertAlmostEqual expected=%f actual=%f delta=%f maxdelta=%f inrange=%d' % (tc_no, expected, actual, delta, maxdelta, inrange)
     return inrange
 
-def checkForEmergenyStop(self, motor, tc_no, wait, direction, oldRBV, TweakValue):
+def checkForEmergenyStop(self, motor, tc_no, wait, direction, oldDRBV, TweakDvalue):
     outOfRange = 0
     lls = 0
     hls = 0
     outOfRange = 0
-    rbv = epics.caget(motor + '.RBV', use_monitor=False)
+    drbv = epics.caget(motor + '.DRBV', use_monitor=False)
     msta = int(epics.caget(motor + '.MSTA', use_monitor=False))
     dmov = int(epics.caget(motor + '.DMOV', use_monitor=False))
     movn = int(epics.caget(motor + '.MOVN', use_monitor=False))
-    if (rbv - oldRBV) > 2 * TweakValue:
+    if (drbv - oldDRBV) > 2 * TweakDvalue:
         outOfRange = 1
-    if (oldRBV -rbv) > 2 * TweakValue:
+    if (oldDRBV -drbv) > 2 * TweakDvalue:
         outOfRange = -1
     if (msta & self.lib.MSTA_BIT_MINUS_LS):
         lls = 1
     if (msta & self.lib.MSTA_BIT_PLUS_LS):
         hls = 1
 
-    print '%s: wait=%f dmov=%d movn=%d lls=%d hls=%d OOR=%d oldRBV=%f rbv=%f' % \
-        (tc_no, wait, dmov, movn, lls, hls, outOfRange, oldRBV, rbv)
+    print '%s: wait=%f dmov=%d movn=%d lls=%d hls=%d OOR=%d oldDRBV=%f drbv=%f' % \
+        (tc_no, wait, dmov, movn, lls, hls, outOfRange, oldDRBV, drbv)
     if ((hls and (direction > 0)) or
         (lls and (direction <= 0)) or
         outOfRange != 0):
-        print '%s STOP=1 CNEN=0 Emergeny stop' % (tc_no)
+        print '%s STOP=1 CNEN=0 Emergency stop' % (tc_no)
         epics.caput(motor + '.STOP', 1)
         epics.caput(motor + '.CNEN', 0)
 
     return (lls, hls, movn, dmov, outOfRange)
 
 
-def waitForStart(self, motor, tc_no, wait_for, direction, oldRBV):
-    TweakValue = epics.caget(motor + '.TWV')
+def waitForStart(self, motor, tc_no, wait_for, direction, oldDRBV):
+    TweakDvalue = epics.caget(motor + '.TWV')
     while wait_for > 0:
         wait_for -= polltime
         (lls, hls, movn, dmov, outOfRange) = checkForEmergenyStop(
-            self, motor, tc_no + 'strt', wait_for, direction, oldRBV, TweakValue)
-        rbv = epics.caget(motor + '.RBV')
+            self, motor, tc_no + 'strt', wait_for, direction, oldDRBV, TweakDvalue)
+        drbv = epics.caget(motor + '.DRBV')
         if movn and not dmov:
             return True
         time.sleep(polltime)
@@ -79,12 +80,11 @@ def waitForErrRst(motor, tc_no, wait_for_ErrRst):
         wait_for_ErrRst -= polltime
     return False
 
-def waitForStop(self, motor, tc_no, wait_for_stop, direction, oldRBV):
+def waitForStop(self, motor, tc_no, wait_for_stop, direction, oldDRBV, TweakDvalue):
     while wait_for_stop > 0:
         wait_for_stop -= polltime
-        TweakValue = epics.caget(motor + '.TWV')
         (lls, hls, movn, dmov, outOfRange) = checkForEmergenyStop(
-            self, motor, tc_no + 'stop', wait_for_stop, direction, oldRBV, TweakValue)
+            self, motor, tc_no + 'stop', wait_for_stop, direction, oldDRBV, TweakDvalue)
 
         if ((hls and (direction > 0)) or
             (lls and (direction <= 0)) or
@@ -97,6 +97,99 @@ def waitForStop(self, motor, tc_no, wait_for_stop, direction, oldRBV):
         wait_for_stop -= polltime
     return False
 
+def tweakToLimit(self, motor, tc_no, direction):
+    oldmsta = int(epics.caget(motor + '.MSTA',  use_monitor=False))
+    if (oldmsta & self.lib.MSTA_BIT_MINUS_LS):
+        oldLLS = 1
+    else:
+        oldLLS = 0
+    if (oldmsta & self.lib.MSTA_BIT_PLUS_LS):
+        oldHLS = 1
+    else:
+        oldHLS = 0
+
+    print '%s tweakToLimit direction=%f Amplifier=%x oldLLS=%d oldHLS=%d' % \
+      (tc_no, direction, oldmsta & self.lib.MSTA_BIT_AMPON, oldLLS, oldHLS)
+
+    assert(direction)
+    oldDRBV = epics.caget(motor + '.DRBV', timeout=5)
+    old_high_limit = epics.caget(motor + '.HLM', timeout=5)
+    old_low_limit = epics.caget(motor + '.LLM', timeout=5)
+    # switch off the soft limits, save the dvalues
+    if oldDRBV > 0:
+        epics.caput(motor + '.LLM', 0.0)
+        epics.caput(motor + '.HLM', 0.0)
+    else:
+        epics.caput(motor + '.HLM', 0.0)
+        epics.caput(motor + '.LLM', 0.0)
+
+    epics.caput(motor + '.CNEN', 1)
+    # Step through the range in 20 steps or so
+    deltaToMove = (old_high_limit - old_low_limit) / 20
+    assert(deltaToMove)
+    maxDeltaAfterMove = deltaToMove / 2
+    # soft limit range + 50%
+    maxTweaks = (old_high_limit - old_low_limit) * 1.5 / deltaToMove
+    assert(maxTweaks > 0)
+    stopTheLoop = 0
+    count = 0
+    lls = 0
+    hls = 0
+    while not stopTheLoop:
+        oldDRBV = epics.caget(motor + '.DRBV', timeout=5)
+        if direction > 0:
+            destination = oldDRBV + deltaToMove
+        else:
+            destination = oldDRBV - deltaToMove
+        print '%s Tweak the motor oldDRBV=%f destination=%f deltaToMove=%f' \
+            % (tc_no, oldDRBV, destination, deltaToMove)
+
+        epics.caput(motor + '.DVAL', destination)
+
+        ret1 = waitForStart(self, motor, tc_no, 0.4, direction, oldDRBV)
+        ret2 = waitForStop(self, motor, tc_no, 10.0, direction, oldDRBV, deltaToMove)
+        msta = int(epics.caget(motor + '.MSTA', timeout=5))
+        if (msta & self.lib.MSTA_BIT_MINUS_LS):
+            lls = 1
+            if oldLLS and direction > 0:
+                print '%s oldLLS=%d lls=%d' % (tc_no, oldLLS, lls)
+            else:
+                stopTheLoop = 1
+        if (msta & self.lib.MSTA_BIT_PLUS_LS):
+            hls = 1
+            if oldHLS and direction <= 0:
+                print '%s oldHLS=%d hls=%d' % (tc_no, oldHLS, hls)
+            else:
+                stopTheLoop = 1
+
+        drbv = epics.caget(motor + '.DRBV', timeout=5)
+        print '%s STOP=1 CNEN=0 start=%d stop=%d drbv=%f lls=%d hls=%d' \
+            % (tc_no, ret1, ret2, drbv, lls, hls)
+        epics.caput(motor + '.STOP', 1)
+        count += 1
+        if (count > maxTweaks):
+            stopTheLoop = 1
+            
+        inPosition = calcAlmostEqual(motor, tc_no, destination, drbv, maxDeltaAfterMove)
+        if not inPosition:
+            stopTheLoop = 1
+            print '%s STOP=1 CNEN=0 Emergeny stop' % (tc_no)
+            epics.caput(motor + '.STOP', 1)
+            epics.caput(motor + '.CNEN', 0)
+
+        print '%s count=%d maxTweaks=%d stopTheLoop=%d inPosition=%d' \
+            % (tc_no, count, maxTweaks, stopTheLoop, inPosition)
+            
+    # Put back the limits
+    epics.caput(motor + '.LLM', old_low_limit)
+    epics.caput(motor + '.HLM', old_high_limit)
+
+    if direction > 0:
+        assert(hls)
+    else:
+        assert(lls)
+
+
 class Test(unittest.TestCase):
     lib = motor_lib()
     motor = os.getenv("TESTEDMOTORAXIS")
@@ -104,11 +197,22 @@ class Test(unittest.TestCase):
     saved_HLM = epics.caget(motor + '.HLM')
     saved_LLM = epics.caget(motor + '.LLM')
     saved_CNEN = epics.caget(motor + '.CNEN')
-    TweakValue = epics.caget(motor + '.TWV')
+    TweakDvalue = epics.caget(motor + '.TWV')
+
+    # VAL matches RBV, especially after IOC reboot
+    def test_TC_090(self):
+        tc_no = "TC-090-RBV-matches-VAL"
+        print '%s' % tc_no
+        motor = self.motor
+        rbv = epics.caget(motor + '.RBV', use_monitor=False)
+        val = epics.caget(motor + '.VAL', use_monitor=False)
+        maxdelta = self.TweakDvalue
+        print '%s RBV=%f VAL=%f, maxdelta=%f' % (tc_no, rbv, val, maxdelta)
+        assert calcAlmostEqual(motor, tc_no, rbv, val, maxdelta)
 
     # TWF/TWR
-    def test_TC_091(self):
-        tc_no = "TC-091-Tweak"
+    def test_TC_092(self):
+        tc_no = "TC-092-Tweak"
         print '%s' % tc_no
         motor = self.motor
         print '%s -ErrRst' % tc_no
@@ -117,10 +221,10 @@ class Test(unittest.TestCase):
         print '%s CNEN=1' % tc_no
         epics.caput(motor + '.CNEN', 1)
 
-        oldRBV = epics.caget(motor + '.RBV', use_monitor=False)
+        oldDRBV = epics.caget(motor + '.DRBV', use_monitor=False)
         old_high_limit = epics.caget(motor + '.HLM')
         old_low_limit = epics.caget(motor + '.LLM')
-        if oldRBV > 0:
+        if oldDRBV > 0:
             epics.caput(motor + '.LLM', 0.0)
             epics.caput(motor + '.HLM', 0.0)
         else:
@@ -131,17 +235,17 @@ class Test(unittest.TestCase):
         direction = 0
         if (msta & self.lib.MSTA_BIT_PLUS_LS):
             direction = -1
-            destination = oldRBV - self.TweakValue
+            destination = oldDRBV - self.TweakDvalue
         else:
             direction = +1
-            destination = oldRBV + self.TweakValue
+            destination = oldDRBV + self.TweakDvalue
 
-        print '%s Tweak the motor oldRBV=%f destination=%f' % (tc_no, oldRBV, destination)
+        print '%s Tweak the motor oldDRBV=%f destination=%f' % (tc_no, oldDRBV, destination)
 
-        epics.caput(motor + '.VAL', destination)
+        epics.caput(motor + '.DVAL', destination)
 
-        ret1 = waitForStart(self, motor, tc_no, 0.4, direction, oldRBV)
-        ret2 = waitForStop(self, motor, tc_no, 10.0, direction, oldRBV)
+        ret1 = waitForStart(self, motor, tc_no, 0.4, direction, oldDRBV)
+        ret2 = waitForStop(self, motor, tc_no, 10.0, direction, oldDRBV, self.TweakDvalue)
         msta = int(epics.caget(motor + '.MSTA'))
         print '%s STOP=1 CNEN=0 start=%d stop=%d' % (tc_no, ret1, ret2)
         epics.caput(motor + '.STOP', 1)
@@ -149,12 +253,26 @@ class Test(unittest.TestCase):
 
         epics.caput(motor + '.LLM', old_low_limit)
         epics.caput(motor + '.HLM', old_high_limit)
-        ReadBackValue = epics.caget(motor + '.RBV', use_monitor=False)
+        ReadBackDVAL = epics.caget(motor + '.DRBV', use_monitor=False)
         print '%s destination=%d postion=%f' % (
-            tc_no, destination, ReadBackValue)
+            tc_no, destination, ReadBackDVAL)
         #self.assertEqual(True, ret1, 'waitForStart return True')
         self.assertEqual(True, ret2, 'waitForStop return True')
-        maxdelta = self.TweakValue * 2
-        print '%s destination=%f ReadBackValue=%f, maxdelta=%f' % (tc_no, destination, ReadBackValue, maxdelta)
-        assert calcAlmostEqual(motor, tc_no, destination, ReadBackValue, maxdelta)
+        maxdelta = self.TweakDvalue * 2
+        print '%s destination=%f ReadBackDVAL=%f, maxdelta=%f' % (tc_no, destination, ReadBackDVAL, maxdelta)
+        assert calcAlmostEqual(motor, tc_no, destination, ReadBackDVAL, maxdelta)
         #assert False
+
+    def test_TC_093(self):
+        tc_no = "TC-093-Tweak-to-HLS"
+        print '%s' % tc_no
+        motor = self.motor
+        direction = 1
+        tweakToLimit(self, motor, tc_no, direction)
+
+    def test_TC_094(self):
+        tc_no = "TC-094-Tweak-to-LLS"
+        print '%s' % tc_no
+        motor = self.motor
+        direction = -1
+        tweakToLimit(self, motor, tc_no, direction)
