37e262a9ad1c9989d052a4d2f0248ede0af206fb
Torsten Bögershausen <torsten.bogershausen@esss.se>
2017-05-18 09:21:43 +0200

    EthercatMC: Add test/startup/st.DualAxis.cmd
    The test crate is going to be rebuild into a dual-axis system, so add
    a start file.

---
+++ test/startup/st.DualAxis.cmd
@@ -0,0 +1,51 @@
+require asyn,4.31
+require axis,USER
+
+epicsEnvSet("MOTOR_PORT",    "$(SM_MOTOR_PORT=MCU1)")
+
+epicsEnvSet("IPADDR",        "$(SM_IPADDR=127.0.0.1)")
+epicsEnvSet("IPPORT",        "$(SM_IPPORT=5000)")
+epicsEnvSet("ASYN_PORT",     "$(SM_ASYN_PORT=MC_CPU1)")
+#epicsEnvSet("AXISCONFIG",    "cfgFile=../iocBoot/iocEthercatMC/SolAxis-48-1.cfg")
+#epicsEnvSet("AXISCONFIG",    "getDebugText=getAxisDebugInfoData(1)")
+epicsEnvSet("AXISCONFIG",    "")
+epicsEnvSet("PREFIX",        "$(SM_PREFIX=IOC:)")
+epicsEnvSet("MOTOR_NAME",    "$(SM_MOTOR_NAME=m1)")
+epicsEnvSet("R",             "$(SM_R=m1-)")
+epicsEnvSet("AXIS_NO",       "$(SM_AXIS_NO=1)")
+epicsEnvSet("DESC",          "$(SM_DESC=motor1)")
+epicsEnvSet("EGU",           "$(SM_EGU=mm)")
+epicsEnvSet("PREC",          "$(SM_PREC=3)")
+epicsEnvSet("VELO",          "$(SM_VELO=10.0)")
+epicsEnvSet("JVEL",          "$(SM_JVEL=5.0)")
+epicsEnvSet("JAR",           "$(SM_JAR=21)")
+epicsEnvSet("ACCL",          "$(SM_ACCL=0.4)")
+epicsEnvSet("MRES",          "$(SM_MRES=0.001)")
+epicsEnvSet("ERES",          "$(SM_MRES=0.0)")
+epicsEnvSet("RDBD",          "$(SM_RDBD=0.1)")
+epicsEnvSet("NTMF",          "$(SM_NTMF=3)")
+epicsEnvSet("MRES",          "$(SM_MRES=0.01)")
+epicsEnvSet("DLLM",          "$(SM_DLLM=15)")
+epicsEnvSet("DHLM",          "$(SM_DHLM=170)")
+
+epicsEnvSet("HOMEPROC",      "$(SM_HOMEPROC=1)")
+epicsEnvSet("HOMEPOS",       "$(SM_HOMEPOS=0.0)")
+epicsEnvSet("HVELTO",        "$(SM_HVELTP=9)")
+epicsEnvSet("HVELFRM",       "$(SM_HVELTP=4)")
+epicsEnvSet("HOMEACC",       "$(SM_HOMEACC=24)")
+epicsEnvSet("HOMEDEC",       "$(SM_HOMEDEC=26)")
+
+< EthercatMCController.cmd
+< EthercatMCAxis.cmd
+
+#############################
+Axis 2
+
+epicsEnvSet("MOTOR_NAME",    "$(SM_MOTOR_NAME=m2)")
+epicsEnvSet("R",             "$(SM_R=m2-)")
+epicsEnvSet("AXIS_NO",       "$(SM_AXIS_NO=2)")
+epicsEnvSet("DESC",          "$(SM_DESC=motor2)")
+
+epicsEnvSet("DLLM",          "$(SM_DLLM=15)")
+epicsEnvSet("DHLM",          "$(SM_DHLM=165)")
+< EthercatMCAxis.cmd
