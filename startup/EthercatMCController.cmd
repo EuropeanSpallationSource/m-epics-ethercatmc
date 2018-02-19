1ed16548431d48d0a4eb425a9fd72e0b5ccb838d
Torsten Bögershausen <torsten.bogershausen@esss.se>
2018-02-19 10:45:59 +0100

    EthercatMCController.cmd comments for the trace level bits
    Add a comment what the different trace level bist mean.
    Especially FLOW, and the unofficial INFO

--- startup/EthercatMCController.cmd
+++ startup/EthercatMCController.cmd
@@ -17,6 +17,13 @@ asynOctetSetOutputEos("$(ASYN_PORT)", -1, ";\n")
 asynOctetSetInputEos("$(ASYN_PORT)", -1, ";\n")
 EthercatMCCreateController("$(MOTOR_PORT)", "$(ASYN_PORT)", "32", "200", "1000")
 
+#define ASYN_TRACE_ERROR     0x0001
+#define ASYN_TRACEIO_DEVICE  0x0002
+#define ASYN_TRACEIO_FILTER  0x0004
+#define ASYN_TRACEIO_DRIVER  0x0008
+#define ASYN_TRACE_FLOW      0x0010
+#define ASYN_TRACE_WARNING   0x0020
+#define ASYN_TRACE_INFO      0x0040
 
 asynSetTraceMask("$(ASYN_PORT)", -1, 0x41)
 asynSetTraceIOMask("$(ASYN_PORT)", -1, 2)
