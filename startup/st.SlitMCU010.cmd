f68c60b59673899197c140196b762a396c5c3dc5
Torsten Bögershausen <torsten.bogershausen@esss.se>
2018-03-13 13:26:25 +0100

    Ad startup files for SlitMCU010
    Add start scripts which allow to commission a dual axis crate
    with incremental encoders in a "slit way":
    Position 0 is in the middle of the axis, in the sense that the
    edge of the wagon(s) simulates the edge of the blades.
    
    Moving both axes to postion 0 will look like a closed slit at postion 0,
    which is in the middle of the beam.

---
+++ startup/st.SlitMCU010.cmd
@@ -0,0 +1,35 @@
+require asyn,4.31
+require EthercatMC,USER
+
+epicsEnvSet("MOTOR_PORT",    "$(SM_MOTOR_PORT=MCU1)")
+
+epicsEnvSet("IPADDR",        "$(SM_IPADDR=127.0.0.1)")
+epicsEnvSet("IPPORT",        "$(SM_IPPORT=5000)")
+epicsEnvSet("ASYN_PORT",     "$(SM_ASYN_PORT=MC_CPU1)")
+epicsEnvSet("PREFIX",        "$(SM_PREFIX=IOC:)")
+epicsEnvSet("MOTOR_NAME",    "$(SM_MOTOR_NAME=m1)")
+epicsEnvSet("M",             "$(SM_M=m1)")
+epicsEnvSet("R",             "$(SM_R=m1-)")
+epicsEnvSet("AXIS_NO",       "$(SM_AXIS_NO=1)")
+epicsEnvSet("DESC",          "$(SM_DESC=Lower=Right)")
+epicsEnvSet("EGU",           "$(SM_EGU=mm)")
+epicsEnvSet("PREC",          "$(SM_PREC=3)")
+
+# And this reads the input of an incremental encoder terminal
+# on the EtherCAT bus. Works with the simulator.
+# For real terminals the adresses must be adapted
+epicsEnvSet("AXISCONFIG",    "stepSize=1.0;cfgFile=./SlitAxisMCU010-1.cfg;encoder=ADSPORT=501/.ADR.16#3040010,16#80000049,2,2;HomProc=1;HomPos=0.0")
+
+< EthercatMCController.cmd
+< EthercatMCAxis.cmd
+< EthercatMCAxisdebug.cmd
+
+epicsEnvSet("AXISCONFIG",    "stepSize=1.0;cfgFile=./SlitAxisMCU010-2.cfg;encoder=ADSPORT=501/.ADR.16#3040010,16#8000004F,2,2;HomProc=2;HomPos=172")
+
+epicsEnvSet("MOTOR_NAME",    "$(SM_MOTOR_NAME=m2)")
+epicsEnvSet("M",             "$(SM_M=m2)")
+epicsEnvSet("R",             "$(SM_R=m2-)")
+epicsEnvSet("AXIS_NO",       "$(SM_AXIS_NO=2)")
+epicsEnvSet("DESC",          "$(SM_DESC=Upper=Left)")
+< EthercatMCAxis.cmd
+< EthercatMCAxisdebug.cmd
