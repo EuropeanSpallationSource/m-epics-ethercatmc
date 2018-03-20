8ca07bc0f8bdbc09d98022776d3beb6d97afc39c
Torsten Bögershausen <torstenbogershausen@esss.lu.se>
2018-03-20 06:55:16 +0100

    Add run-ADS-ioc.sh
    Be prepared to run against the ads driver, instead of asyn's IPPort
    on the IOC together with EpicsComModule in the motion controller.
    Compile & link against ads, if defined.

--- startup/EthercatMCController.cmd
+++ startup/EthercatMCController.cmd
@@ -12,9 +12,16 @@
 # @field IPPORT
 # @type  INTEGER
 
+## One of the 2 needs to be done, either drvAsynIPPortConfigure+Eos
+## Or
+## adsAsynPortDriverConfigure (without Eos.
+## Note that ./run-ADS-ioc.sh will patch this file as needed
 drvAsynIPPortConfigure("$(ASYN_PORT)","$(IPADDR):$(IPPORT)",0,0,0)
 asynOctetSetOutputEos("$(ASYN_PORT)", -1, ";\n")
 asynOctetSetInputEos("$(ASYN_PORT)", -1, ";\n")
+
+#adsAsynPortDriverConfigure("$(ASYN_PORT)","$(IPADDR)","$(AMSID)",852,1000,0,0,50,100,1000,0)
+
 EthercatMCCreateController("$(MOTOR_PORT)", "$(ASYN_PORT)", "32", "200", "1000")
 
 #define ASYN_TRACE_ERROR     0x0001
--- startup/st.SlitMCU010.cmd
+++ startup/st.SlitMCU010.cmd
@@ -6,39 +6,44 @@ epicsEnvSet("MOTOR_PORT",    "$(SM_MOTOR_PORT=MCU1)")
 epicsEnvSet("IPADDR",        "$(SM_IPADDR=127.0.0.1)")
 epicsEnvSet("IPPORT",        "$(SM_IPPORT=5000)")
 epicsEnvSet("ASYN_PORT",     "$(SM_ASYN_PORT=MC_CPU1)")
-epicsEnvSet("PREFIX",        "$(SM_PREFIX=IOC:)")
+epicsEnvSet("PREFIX",        "$(SM_PREFIX=HZB-V20:MC-)")
+epicsEnvSet("P",             "$(PREFIX)")
 epicsEnvSet("EGU",           "$(SM_EGU=mm)")
 epicsEnvSet("PREC",          "$(SM_PREC=3)")
+< EthercatMCController.cmd
+
+# 2 physical axes
+epicsEnvSet("M1",           "$(SM_mXp=MCU-01:m1)")
+epicsEnvSet("R1",           "$(SM_mXp=MCU-01:m1-)")
+epicsEnvSet("M2",           "$(SM_mXp=MCU-01:m2)")
+epicsEnvSet("R2",           "$(SM_mXp=MCU-01:m1-)")
+
+# Logical axes and slit
+epicsEnvSet("SLIT",          "$(SM_SLIT=SltH-)")
+epicsEnvSet("mXp",           "$(SM_mXp=MCU-01:m1)")
+epicsEnvSet("mXn",           "$(SM_mXp=m2)")
 
 epicsEnvSet("MOTOR_NAME",    "$(SM_MOTOR_NAME=m2)")
-epicsEnvSet("M",             "$(SM_M=m2)")
-epicsEnvSet("R",             "$(SM_R=m2-)")
+epicsEnvSet("M",             "$(M2)")
+epicsEnvSet("R",             "$(R2)")
 epicsEnvSet("AXIS_NO",       "$(SM_AXIS_NO=1)")
 epicsEnvSet("DESC",          "$(SM_DESC=Lower=Right)")
-
 # And this reads the input of an incremental encoder terminal
 # on the EtherCAT bus. Works with the simulator.
 # For real terminals the adresses must be adapted
 epicsEnvSet("AXISCONFIG",    "stepSize=1.0;cfgFile=./SlitAxisMCU010-1.cfg;encoder=ADSPORT=501/.ADR.16#3040010,16#80000049,2,2;HomProc=1;HomPos=0.0")
 
-< EthercatMCController.cmd
 < EthercatMCAxis.cmd
-#< EthercatMCAxisdebug.cmd
 
 epicsEnvSet("AXISCONFIG",    "stepSize=1.0;cfgFile=./SlitAxisMCU010-2.cfg;encoder=ADSPORT=501/.ADR.16#3040010,16#8000004F,2,2;HomProc=2;HomPos=172")
 
 epicsEnvSet("MOTOR_NAME",    "$(SM_MOTOR_NAME=m1)")
-epicsEnvSet("M",             "$(SM_M=m1)")
-epicsEnvSet("R",             "$(SM_R=m1-)")
+epicsEnvSet("M",             "$(M1)")
+epicsEnvSet("R",             "$(R1)")
 epicsEnvSet("AXIS_NO",       "$(SM_AXIS_NO=2)")
 epicsEnvSet("DESC",          "$(SM_DESC=Upper=Left)")
 < EthercatMCAxis.cmd
-#< EthercatMCAxisdebug.cmd
 
-
-epicsEnvSet("P",             "$(PREFIX)")
-epicsEnvSet("SLIT",          "$(SM_SLIT=SltH-)")
-epicsEnvSet("mXp",            "$(SM_mXp=m1)")
-epicsEnvSet("mXn",            "$(SM_mXp=m2)")
 
 < EthercatMC2slit.cmd
+#########################
