e5807c28c816e62c66914aaa8bac04d358cf5a7f
Torsten Bögershausen <torsten.bogershausen@esss.se>
2018-04-10 06:54:06 +0200

    Rename ESTIARND PVs

--- test/startup/st.ESTIARND.cmd
+++ test/startup/st.ESTIARND.cmd
@@ -6,20 +6,21 @@ epicsEnvSet("MOTOR_PORT",    "$(SM_MOTOR_PORT=MCU1)")
 epicsEnvSet("IPADDR",        "$(SM_IPADDR=127.0.0.1)")
 epicsEnvSet("IPPORT",        "$(SM_IPPORT=5000)")
 epicsEnvSet("ASYN_PORT",     "$(SM_ASYN_PORT=MC_CPU1)")
-epicsEnvSet("PREFIX",        "$(SM_PREFIX=IOC:)")
+epicsEnvSet("PREFIX",        "$(SM_PREFIX=PSI-ESTIARND:MC-MCU-01:)")
+< EthercatMCController.cmd
+
+
+# Common to all axes
+epicsEnvSet("EGU",           "$(SM_EGU=mm)")
+epicsEnvSet("PREC",          "$(SM_PREC=3)")
+###############################################
+epicsEnvSet("AXISCONFIG",    "HomProc=1")
 epicsEnvSet("MOTOR_NAME",    "$(SM_MOTOR_NAME=m1)")
 epicsEnvSet("M",             "$(SM_M=m1)")
 epicsEnvSet("R",             "$(SM_R=m1-)")
 epicsEnvSet("AXIS_NO",       "$(SM_AXIS_NO=1)")
 epicsEnvSet("DESC",          "$(SM_DESC=Planetary)")
-epicsEnvSet("EGU",           "$(SM_EGU=mm)")
-epicsEnvSet("PREC",          "$(SM_PREC=3)")
 
-# And this reads the input of an incremental encoder terminal
-# on the EtherCAT bus. Works with the simulator.
-# For real terminals the adresses must be adapted
-epicsEnvSet("AXISCONFIG",    "HomProc=1")
-< EthercatMCController.cmd
 < EthercatMCAxis.cmd
 #< EthercatMCAxisdebug.cmd
 
