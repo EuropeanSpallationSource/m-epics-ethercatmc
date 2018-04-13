17faa2bde939b1d1e993f54798a07f6efb01dadb
Torsten Bögershausen <torsten.bogershausen@esss.se>
2018-04-13 12:29:54 +0200

    Generate ../motor5x.opi and use it for ESTIARND
    Re-write gen6x.sh into genx.sh, which can generate any number of
    motors.
    USe it to generate a 5 motor version, motor5x.opi, and use it for ESTIARND.

--- EthercatMCExApp/op/Boy/tools/genx.sh
+++ EthercatMCExApp/op/Boy/tools/genx.sh
@@ -1,11 +1,20 @@
 #!/bin/sh
 
+if test -z "$1"; then
+	echo >&2 "$0 <numberOfMotors>"
+	exit 1
+fi
+if test "$1" -eq 0; then
+	echo >&2 "$0 <numberOfMotors>"
+	exit 1
+fi
+N="$1"
 OPIS=..
-FILE=motor6x.opi
+FILE=motor${N}x.opi
 
 cp $OPIS/motorx.start $OPIS/$FILE &&
-	i=0
-while test "$i" -lt 6; do
+i=0
+while test $i -lt $N; do
 	x=$(($i * 120))
 	./shiftopi.py --shiftx $x --shiftm $i <$OPIS/motorx.mid >>$OPIS/$FILE
 	i=$(($i + 1))
