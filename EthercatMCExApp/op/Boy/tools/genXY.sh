cd26b174a44cee7fc5282dc1ceaaceb9019debb8
Torsten Bögershausen <torsten.bogershausen@esss.se>
2018-04-16 08:27:19 +0200

    genXY.sh: Make it possible to generate e.g. motor4x3.opi
    Add an Y-dimension:
    Now we can generate 2-dimensional displays

--- EthercatMCExApp/op/Boy/tools/genXY.sh
+++ EthercatMCExApp/op/Boy/tools/genXY.sh
@@ -1,25 +1,45 @@
 #!/bin/sh
 
+# Number of motors in Y direction
+Y=1
+OPIS=..
+
 if test -z "$1"; then
-	echo >&2 "$0 <numberOfMotors>"
+	echo >&2 "$0 <numberOfMotorsX>"
 	exit 1
 fi
 if test "$1" -eq 0; then
-	echo >&2 "$0 <numberOfMotors>"
+	echo >&2 "$0 <numberOfMotorsX>"
 	exit 1
 fi
-N="$1"
-OPIS=..
-FILE=motor${N}x.opi
+X="$1"
+FILE=motor${X}x.opi
+
+if test -n "$2"; then
+	if test "$2" -eq 0; then
+		echo >&2 "$0 <numberOfMotorsY>"
+		exit 1
+	fi
+	Y=$2
+	FILE=motor${X}x${Y}.opi
+fi
 
 cp $OPIS/motorx.start $OPIS/$FILE &&
-i=0
-while test $i -lt $N; do
-	x=$(($i * 120))
-	./shiftopi.py --shiftx $x --shiftm $i <$OPIS/motorx.mid >>$OPIS/$FILE
-	i=$(($i + 1))
+iy=0
+while test $iy -lt $Y; do
+	ix=0
+	y=$(($iy * 173))
+	while test $ix -lt $X; do
+		x=$(($ix * 120))
+		im=$(($ix + ($iy * X)))
+		cmd=$(echo ./shiftopi.py --shiftx $x --shifty $y --shiftm $im)
+		echo cmd=$cmd
+		eval $cmd <$OPIS/motorx.mid >>$OPIS/$FILE
+		ix=$(($ix + 1))
+	done &&
+  iy=$(($iy + 1))
 done &&
-cat $OPIS/motorx.end  >>$OPIS/$FILE
+	cat $OPIS/motorx.end  >>$OPIS/$FILE
 
 
 	
