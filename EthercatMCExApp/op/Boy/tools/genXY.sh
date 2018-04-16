8b383bb0785bac318879106cd54e4dc84c3c6bf1
Torsten Bögershausen <torsten.bogershausen@esss.se>
2018-04-16 10:35:39 +0200

    Allow genXY.sh 4 x 3
    We may want to have a non-rectangle layout:
    If the layout is "4 x 3" then the user must specify the "x"

--- EthercatMCExApp/op/Boy/tools/genXY.sh
+++ EthercatMCExApp/op/Boy/tools/genXY.sh
@@ -12,29 +12,36 @@ if test "$1" -eq 0; then
   echo >&2 "$0 <numberOfMotorsX>"
   exit 1
 fi
-X="$1"
+X=$1
+shift
 FILE=motor${X}x.opi
 
-if test -n "$2"; then
-  if test "$2" -eq 0; then
-    echo >&2 "$0 <numberOfMotorsY>"
-    exit 1
+#Do we have e.g. 4 x 3
+if test "$1" = x; then
+  shift
+  if test -n "$1"; then
+    if test "$1" -eq 0; then
+      echo >&2 "$0 <numberOfMotorsY>"
+      exit 1
+    fi
+    Y=$1
+		shift
+    FILE=motor${X}x${Y}.opi
   fi
-  Y=$2
-  FILE=motor${X}x${Y}.opi
 fi
 
 cp motorx.start $OPIS/$FILE &&
 iy=0
+im=0
 while test $iy -lt $Y; do
   ix=0
   y=$(($iy * 173))
   while test $ix -lt $X; do
     x=$(($ix * 120))
-    im=$(($ix + ($iy * X)))
     cmd=$(echo ./shiftopi.py --shiftx $x --shifty $y --shiftm $im)
     echo cmd=$cmd
     eval $cmd <motorx.mid >>$OPIS/$FILE
+    im=$(($im + 1))
     ix=$(($ix + 1))
   done &&
   iy=$(($iy + 1))
