12703d28fce48044aa3ac8f1f9927344cbeb9e26
Torsten Bögershausen <torsten.bogershausen@esss.se>
2018-04-24 14:34:45 +0200

    Configure vertical slit and axes5..8 for HZB

--- test/startup/st.HZBV20-SoftSlit-Sim.cmd
+++ test/startup/st.HZBV20-SoftSlit-Sim.cmd
@@ -21,14 +21,14 @@ epicsEnvSet("AXISCONFIG",    "cfgFile=./SlitAxisMCU010-1.cfg")
 epicsEnvSet("MOTOR_NAME",    "MC-MCU-01:m1")
 epicsEnvSet("R",             "MC-MCU-01:m1-")
 epicsEnvSet("AXIS_NO",       "1")
-epicsEnvSet("DESC",          "V low blade")
+epicsEnvSet("DESC",          "H low blade")
 < EthercatMCAxis.cmd
 
 epicsEnvSet("AXISCONFIG",    "cfgFile=./SlitAxisMCU010-2.cfg")
 epicsEnvSet("MOTOR_NAME",    "MC-MCU-01:m2")
 epicsEnvSet("R",             "MC-MCU-01:m2-")
 epicsEnvSet("AXIS_NO",       "2")
-epicsEnvSet("DESC",          "V high Blade")
+epicsEnvSet("DESC",          "H high Blade")
 < EthercatMCAxis.cmd
 
 ## Logical axes and slit
@@ -39,3 +39,57 @@ epicsEnvSet("mXn",           "MC-MCU-01:m1")
 ## Slit
 < EthercatMC2slit.cmd
 #########################
+
+# Slit Vertical
+#########################
+epicsEnvSet("AXISCONFIG",    "cfgFile=./SlitAxisMCU010-1.cfg")
+epicsEnvSet("MOTOR_NAME",    "MC-MCU-01:m3")
+epicsEnvSet("R",             "MC-MCU-01:m3-")
+epicsEnvSet("AXIS_NO",       "3")
+epicsEnvSet("DESC",          "V low blade")
+< EthercatMCAxis.cmd
+
+epicsEnvSet("AXISCONFIG",    "cfgFile=./SlitAxisMCU010-2.cfg")
+epicsEnvSet("MOTOR_NAME",    "MC-MCU-01:m4")
+epicsEnvSet("R",             "MC-MCU-01:m4-")
+epicsEnvSet("AXIS_NO",       "4")
+epicsEnvSet("DESC",          "V high Blade")
+< EthercatMCAxis.cmd
+
+## Logical axes and slit
+epicsEnvSet("SLIT",          "MC-SLT-01:SltV-")
+epicsEnvSet("mXp",           "MC-MCU-01:m4")
+epicsEnvSet("mXn",           "MC-MCU-01:m3")
+
+## Slit
+< EthercatMC2slit.cmd
+#########################
+
+
+epicsEnvSet("AXISCONFIG",    "cfgFile=./SlitAxisMCU010-1.cfg")
+epicsEnvSet("MOTOR_NAME",    "MC-MCU-01:m5")
+epicsEnvSet("R",             "MC-MCU-01:m5-")
+epicsEnvSet("AXIS_NO",       "5")
+epicsEnvSet("DESC",          "Height 1")
+< EthercatMCAxis.cmd
+
+epicsEnvSet("AXISCONFIG",    "cfgFile=./SlitAxisMCU010-1.cfg")
+epicsEnvSet("MOTOR_NAME",    "MC-MCU-01:m6")
+epicsEnvSet("R",             "MC-MCU-01:m6-")
+epicsEnvSet("AXIS_NO",       "6")
+epicsEnvSet("DESC",          "Omega 2")
+< EthercatMCAxis.cmd
+
+epicsEnvSet("AXISCONFIG",    "cfgFile=./SlitAxisMCU010-1.cfg")
+epicsEnvSet("MOTOR_NAME",    "MC-MCU-01:m7")
+epicsEnvSet("R",             "MC-MCU-01:m7-")
+epicsEnvSet("AXIS_NO",       "7")
+epicsEnvSet("DESC",          "Kappa")
+< EthercatMCAxis.cmd
+
+epicsEnvSet("AXISCONFIG",    "cfgFile=./SlitAxisMCU010-1.cfg")
+epicsEnvSet("MOTOR_NAME",    "MC-MCU-01:m8")
+epicsEnvSet("R",             "MC-MCU-01:m8-")
+epicsEnvSet("AXIS_NO",       "8")
+epicsEnvSet("DESC",          "Lin3")
+< EthercatMCAxis.cmd
