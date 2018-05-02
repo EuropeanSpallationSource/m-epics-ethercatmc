14cbe4524240de4a30dbb1e6e2bd4028c01d37a8
Torsten Bögershausen <torsten.bogershausen@esss.se>
2018-05-02 08:18:21 +0200

    Add  test/startup/st.SoftSlitMCU011.cmd

--- test/startup/st.mcu011-sim.cmd
+++ test/startup/st.mcu011-sim.cmd
@@ -11,16 +11,10 @@ epicsEnvSet("ASYN_PORT",     "$(SM_ASYN_PORT=MC_CPU1)")
 epicsEnvSet("PREFIX",        "$(SM_PREFIX=IOC:)")
 epicsEnvSet("EGU",           "$(SM_EGU=mm)")
 epicsEnvSet("PREC",          "$(SM_PREC=3)")
-
-# And this reads the input of an incremental encoder terminal
-# on the EtherCAT bus. Works with the simulator.
-# For real terminals the adresses must be adapted
-#epicsEnvSet("AXISCONFIG",    "encoder=ADSPORT=501/.ADR.16#3040010,16#80000049,2,2;HomProc=0")
-epicsEnvSet("AXISCONFIG",    "HomProc=0")
-
 < EthercatMCController.cmd
 
 
+epicsEnvSet("AXISCONFIG",    "HomProc=0;cfgFile=./mcu011-sim-1.cfg")
 epicsEnvSet("MOTOR_NAME",    "$(SM_MOTOR_NAME=m1)")
 epicsEnvSet("M",             "$(SM_M=m1)")
 epicsEnvSet("R",             "$(SM_R=m1-)")
@@ -28,6 +22,7 @@ epicsEnvSet("AXIS_NO",       "$(SM_AXIS_NO=1)")
 epicsEnvSet("DESC",          "$(SM_DESC=Y_NEG)")
 < EthercatMCAxis.cmd
 
+epicsEnvSet("AXISCONFIG",    "HomProc=0;cfgFile=./mcu011-sim-2.cfg")
 epicsEnvSet("MOTOR_NAME",    "$(SM_MOTOR_NAME=m2)")
 epicsEnvSet("M",             "$(SM_M=m2)")
 epicsEnvSet("R",             "$(SM_R=m2-)")
@@ -35,6 +30,7 @@ epicsEnvSet("AXIS_NO",       "$(SM_AXIS_NO=2)")
 epicsEnvSet("DESC",          "$(SM_DESC=Y_POS)")
 < EthercatMCAxis.cmd
 
+epicsEnvSet("AXISCONFIG",    "HomProc=0")
 epicsEnvSet("MOTOR_NAME",    "$(SM_MOTOR_NAME=m3)")
 epicsEnvSet("M",             "$(SM_M=m3)")
 epicsEnvSet("R",             "$(SM_R=m3-)")
@@ -42,6 +38,7 @@ epicsEnvSet("AXIS_NO",       "$(SM_AXIS_NO=3)")
 epicsEnvSet("DESC",          "$(SM_DESC=Y_CENTER)")
 < EthercatMCAxis.cmd
 
+epicsEnvSet("AXISCONFIG",    "HomProc=0")
 epicsEnvSet("MOTOR_NAME",    "$(SM_MOTOR_NAME=m4)")
 epicsEnvSet("M",             "$(SM_M=m4)")
 epicsEnvSet("R",             "$(SM_R=m4-)")
