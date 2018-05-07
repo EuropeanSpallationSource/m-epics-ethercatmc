a96ea67e38866a5b9ac8a82425d68342c113f905
Paul Barron <paul.barron@esss.se>
2018-05-07 13:02:21 +0200

    st.mcu200-HZBV20.cmd: Add more axes and corrected axis IDs.
    Add vertical slit as axes 5,6.7,8
    Add axes 9,10,11,12 for the motion stage

--- test/startup/st.mcu200-HZBV20.cmd
+++ test/startup/st.mcu200-HZBV20.cmd
@@ -19,25 +19,25 @@ epicsEnvSet("PREC",          "$(SM_PREC=3)")
 #########################
 epicsEnvSet("AXISCONFIG",    "stepSize=1.0;HomProc=0")
 epicsEnvSet("MOTOR_NAME",    "MC-SLT-01:SltH-Center")
-epicsEnvSet("AXIS_NO",       "3")
+epicsEnvSet("AXIS_NO",       "1")
 epicsEnvSet("DESC",          "H Center")
 < EthercatMCAxis.cmd
 
 epicsEnvSet("AXISCONFIG",    "stepSize=1.0;HomProc=0")
 epicsEnvSet("MOTOR_NAME",    "MC-SLT-01:SltH-Gap")
-epicsEnvSet("AXIS_NO",       "4")
+epicsEnvSet("AXIS_NO",       "2")
 epicsEnvSet("DESC",          "H Gap")
 < EthercatMCAxis.cmd
 
 epicsEnvSet("AXISCONFIG",    "stepSize=1.0;HomProc=0")
 epicsEnvSet("MOTOR_NAME",    "MC-SLT-01:SltH-xp")
-epicsEnvSet("AXIS_NO",       "2")
+epicsEnvSet("AXIS_NO",       "3")
 epicsEnvSet("DESC",          "H Pos Blade")
 < EthercatMCAxis.cmd
 
 epicsEnvSet("AXISCONFIG",    "stepSize=1.0;HomProc=0")
 epicsEnvSet("MOTOR_NAME",    "MC-SLT-01:SltH-xn")
-epicsEnvSet("AXIS_NO",       "1")
+epicsEnvSet("AXIS_NO",       "4")
 epicsEnvSet("DESC",          "H Neg Blade")
 < EthercatMCAxis.cmd
 
@@ -53,27 +53,65 @@ epicsEnvSet("mXn",           "MC-SLT-01:SltH-xn")
 < EthercatMCslitAvoidCollSoftlimits.cmd
 #########################
 
+# Real motors and slit systems
+#########################
+epicsEnvSet("AXISCONFIG",    "stepSize=1.0;HomProc=0")
+epicsEnvSet("MOTOR_NAME",    "MC-SLT-01:SltV-Center")
+epicsEnvSet("AXIS_NO",       "5")
+epicsEnvSet("DESC",          "V Center")
+< EthercatMCAxis.cmd
+
+epicsEnvSet("AXISCONFIG",    "stepSize=1.0;HomProc=0")
+epicsEnvSet("MOTOR_NAME",    "MC-SLT-01:SltV-Gap")
+epicsEnvSet("AXIS_NO",       "6")
+epicsEnvSet("DESC",          "V Gap")
+< EthercatMCAxis.cmd
+
+epicsEnvSet("AXISCONFIG",    "stepSize=1.0;HomProc=0")
+epicsEnvSet("MOTOR_NAME",    "MC-SLT-01:SltV-xp")
+epicsEnvSet("AXIS_NO",       "7")
+epicsEnvSet("DESC",          "V Pos Blade")
+< EthercatMCAxis.cmd
+
+epicsEnvSet("AXISCONFIG",    "stepSize=1.0;HomProc=0")
+epicsEnvSet("MOTOR_NAME",    "MC-SLT-01:SltV-xn")
+epicsEnvSet("AXIS_NO",       "8")
+epicsEnvSet("DESC",          "V Neg Blade")
+< EthercatMCAxis.cmd
+
+#########################
+## Logical axes and slit
+epicsEnvSet("SLIT",          "MC-SLT-01:SltV-")
+epicsEnvSet("mXc",           "Center")
+epicsEnvSet("mXg",           "Gap")
+epicsEnvSet("mXp",           "MC-SLT-01:SltV-xp")
+epicsEnvSet("mXn",           "MC-SLT-01:SltV-xn")
+
+## Slit
+< EthercatMCslitAvoidCollSoftlimits.cmd
+#########################
+
 
 epicsEnvSet("AXISCONFIG",    "stepSize=1.0;HomProc=15")
 epicsEnvSet("MOTOR_NAME",    "MC-MCU-01:m9")
 epicsEnvSet("AXIS_NO",       "9")
 epicsEnvSet("DESC",          "Height 1")
-## XX < EthercatMCAxis.cmd
+< EthercatMCAxis.cmd
 
 epicsEnvSet("AXISCONFIG",    "stepSize=1.0;HomProc=15")
 epicsEnvSet("MOTOR_NAME",    "MC-MCU-01:m10")
-epicsEnvSet("AXIS_NO",       "11")
+epicsEnvSet("AXIS_NO",       "10")
 epicsEnvSet("DESC",          "Omega 2")
-## XX < EthercatMCAxis.cmd
+< EthercatMCAxis.cmd
 
 epicsEnvSet("AXISCONFIG",    "stepSize=1.0;HomProc=15")
 epicsEnvSet("MOTOR_NAME",    "MC-MCU-01:m11")
 epicsEnvSet("AXIS_NO",       "11")
 epicsEnvSet("DESC",          "Kappa")
-## XX < EthercatMCAxis.cmd
+< EthercatMCAxis.cmd
 
 epicsEnvSet("AXISCONFIG",    "stepSize=1.0;HomProc=15")
 epicsEnvSet("MOTOR_NAME",    "MC-MCU-01:m12")
 epicsEnvSet("AXIS_NO",       "12")
 epicsEnvSet("DESC",          "Lin3")
-## XX < EthercatMCAxis.cmd
+< EthercatMCAxis.cmd
