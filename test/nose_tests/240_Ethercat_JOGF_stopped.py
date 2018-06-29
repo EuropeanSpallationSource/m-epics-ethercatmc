03e9d9ffb0bb398fa14621efc111098a223313c6
Torsten Bögershausen <torsten.bogershausen@esss.se>
2018-06-29 08:48:39 +0200

    Add TC 240: Spontanous stop while jogging
    Test if VAL is synchroized with RBV after a spontanous stop while jogging.
    See motorRecord.cc:
    "Motor stopped while jogging and we didn't stop it"

--- test/nose_tests/240_Ethercat_JOGF_stopped.py
+++ test/nose_tests/240_Ethercat_JOGF_stopped.py
@@ -17,12 +17,12 @@ from motor_globals import motor_globals
 # See test/simulator/EtherCAT/hw_motor.h
 AMPLIFIER_LOCKED_TO_BE_OFF_SILENT = 1
 
-def setValueOnSimulator(self, motor, tc_no, var, value):
+def setValueOnAxis(self, motor, tc_no, var, value):
     # Note: The driver will change Sim.this.XXX into
     # Sim.M1.XXX
     var = str(var)
     value = str(value)
-    outStr = 'Sim.this.' + var + '=' + value
+    outStr = 'Main.this.' + var + '=' + value
     print '%s: DbgStrToMCU motor=%s var=%s value=%s outStr=%s' % \
           (tc_no, motor, var, value, outStr)
     assert(len(outStr) < 40)
@@ -33,44 +33,46 @@ class Test(unittest.TestCase):
     lib = motor_lib()
     __g = motor_globals()
     motor = os.getenv("TESTEDMOTORAXIS")
-    motor2 = "IOC:m2"
     hlm = epics.caget(motor + '.HLM')
     llm = epics.caget(motor + '.LLM')
+    saved_DLY  = epics.caget(motor + '.DLY')
     msta             = int(epics.caget(motor + '.MSTA'))
 
     per10_UserPosition  = round((9 * llm + 1 * hlm) / 10)
     per90_UserPosition  = round((1 * llm + 9 * hlm) / 10)
 
     # Assert that motor is homed
-    def test_TC_231(self):
+    def test_TC_241(self):
         motor = self.motor
-        tc_no = "TC-231"
+        tc_no = "TC-241"
         if not (self.msta & self.lib.MSTA_BIT_HOMED):
             self.assertNotEqual(0, self.msta & self.lib.MSTA_BIT_HOMED, 'MSTA.homed (Axis has been homed)')
 
-    # Jog, wait for start, power off, check error, reset error
-    def test_TC_232(self):
+    # Jog, wait for start, stop behind MR
+    def test_TC_242(self):
         motor = self.motor
-        motor2 = self.motor2
-        tc_no = "TC-232-NetYetStarted"
+        tc_no = "TC-242-JOGF_stopped"
         print '%s' % tc_no
 
         if (self.msta & self.lib.MSTA_BIT_HOMED):
-            setValueOnSimulator(self, motor2, tc_no, "usleep", 1700000)
-
-            destination = self.per90_UserPosition
-            res1 = self.lib.move(motor, destination, 60)
-            UserPosition = epics.caget(motor + '.RBV', use_monitor=False)
-            print '%s postion=%f per90_UserPosition=%f' % (
-                tc_no, UserPosition, self.per90_UserPosition)
-
+            epics.caput(motor + '.DLY', 0)
             destination = self.per10_UserPosition
-            res2 = self.lib.move(motor, destination, 60)
+            res1 = self.lib.move(motor, destination, 60)
             UserPosition = epics.caget(motor + '.RBV', use_monitor=False)
             print '%s postion=%f per10_UserPosition=%f' % (
                 tc_no, UserPosition, self.per90_UserPosition)
 
-            setValueOnSimulator(self, motor2, tc_no, "usleep", 0)
+            epics.caput(motor + '.JOGF', 1)
+            ret2 = self.lib.waitForStart(motor, tc_no, 2.0)
+
+            time.sleep(3)
+            setValueOnAxis(self, motor, tc_no, "bExecute", 0)
+            ret3 = self.lib.waitForStop(motor, tc_no, 2.0)
+
+            val = epics.caget(motor + '.VAL')
+            res4 = self.lib.verifyPosition(motor, val)
+            epics.caput(motor + '.DLY', self.saved_DLY)
 
             self.assertNotEqual(res1 == self.__g.SUCCESS, 'move returned SUCCESS')
-            self.assertNotEqual(res2 == self.__g.SUCCESS, 'move returned SUCCESS')
+            #self.assertNotEqual(res2 == self.__g.SUCCESS, 'move returned SUCCESS')
+            self.assertNotEqual(res4 == self.__g.SUCCESS, 'VAL synched with RBV')
