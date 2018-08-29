c82e7e5c38f1e3bdef8cd584fb7a715a7fbe44d8
Torsten Bögershausen <torsten.bogershausen@esss.se>
2018-08-29 07:26:09 +0200

    Rename 2slit_XXX.template
    Make the name(s) of the template files more consistent:
    All should start with EthercatMC:
    EthercatMCApp/Db/2slit_hard.template -> EthercatMCApp/Db/EthercatMCslit_hard.template
    EthercatMCApp/Db/2slit_soft.template -> EthercatMCApp/Db/EthercatMCslit_soft.template
    
    Adjust Makefiles and start scripts

--- startup/EthercatMC2slit.cmd
+++ startup/EthercatMC2slit.cmd
@@ -1,3 +1,3 @@
 
-#dbLoadRecords("2slit_soft.template", "PREFIX=$(PREFIX), MOTOR_NAME=$(MOTOR_NAME), R=$(R), MOTOR_PORT=$(MOTOR_PORT), ASYN_PORT=$(ASYN_PORT), AXIS_NO=$(AXIS_NO), DESC=$(DESC), PREC=$(PREC) ")
-dbLoadRecords("2slit_soft.template", "P=$(P), SLIT=$(SLIT), mXp=$(mXp), mXn=$(mXn) ")
+#dbLoadRecords("EthercatMCslit_soft.template", "PREFIX=$(PREFIX), MOTOR_NAME=$(MOTOR_NAME), R=$(R), MOTOR_PORT=$(MOTOR_PORT), ASYN_PORT=$(ASYN_PORT), AXIS_NO=$(AXIS_NO), DESC=$(DESC), PREC=$(PREC) ")
+dbLoadRecords("EthercatMCslit_soft.template", "P=$(P), SLIT=$(SLIT), mXp=$(mXp), mXn=$(mXn) ")
--- startup/EthercatMC2slit_hard.cmd
+++ startup/EthercatMC2slit_hard.cmd
@@ -1,2 +1,2 @@
-dbLoadRecords("2slit_hard.template", "P=$(P), SLIT=$(SLIT), mXp=$(mXp), mXn=$(mXn), mXc=$(mXc), mXg=$(mXg) ")
+dbLoadRecords("EthercatMCslit_hard.template", "P=$(P), SLIT=$(SLIT), mXp=$(mXp), mXn=$(mXn), mXc=$(mXc), mXg=$(mXg) ")
 
