a954a35a3f3c456457ec42da98034daa1f96ee21
Torsten Bögershausen <torsten.bogershausen@esss.se>
2018-09-26 13:50:09 +0200

    Add startup/ESSIIP-CfgDbg
    Add a start script and config file for the ESSIIP crate.
    Note that we set the soft low limit to 0.0, so that we are
    "in range" after homing".

---
+++ test/startup/st.ESSIIP-CfgDbg.cmd
@@ -0,0 +1,22 @@
+require EthercatMC,USER
+
+epicsEnvSet("MOTOR_PORT",    "$(SM_MOTOR_PORT=MCU1)")
+
+epicsEnvSet("IPADDR",        "$(SM_IPADDR=127.0.0.1)")
+epicsEnvSet("IPPORT",        "$(SM_IPPORT=5000)")
+epicsEnvSet("ASYN_PORT",     "$(SM_ASYN_PORT=MC_CPU1)")
+epicsEnvSet("PREFIX",        "$(SM_PREFIX=IOC:)")
+epicsEnvSet("MOTOR_NAME",    "$(SM_MOTOR_NAME=m1)")
+epicsEnvSet("AXIS_NO",       "$(SM_AXIS_NO=1)")
+epicsEnvSet("DESC",          "$(SM_DESC=Lower=Right)")
+epicsEnvSet("EGU",           "$(SM_EGU=mm)")
+epicsEnvSet("PREC",          "$(SM_PREC=3)")
+
+# And this reads the input of an incremental encoder terminal
+# on the EtherCAT bus. Works with the simulator.
+# For real terminals the adresses must be adapted
+epicsEnvSet("AXISCONFIG",    "adsPort=851;stepSize=1.0;HomProc=1;cfgFile=./ESSIIP-CfgDbg.cfg;encoder=ADSPORT=501/.ADR.16#3040010,16#80000049,2,2")
+
+< EthercatMCController.cmd
+< EthercatMCAxis.cmd
+< EthercatMCAxisdebug.cmd
