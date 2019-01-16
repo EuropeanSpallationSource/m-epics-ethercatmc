3d66dfa099b17984d48847b6d567eb76304e0604
Torsten Bögershausen <torsten.bogershausen@esss.se>
2019-01-16 09:07:16 +0100

    Use crate 019 as a slit demo
    - Add an OPI
    - Adjust the start scripts (0.0 is in the middle of the axis)
    	modified:   EthercatMCExApp/op/Boy/IOC.opi
    	new file:   EthercatMCExApp/op/Boy/motor-4-2.opi
    	modified:   EthercatMCExApp/op/Boy/tools/Makefile
    	modified:   test/startup/st.mcu019-Dbg.cmd

--- test/startup/st.mcu019-Dbg.cmd
+++ test/startup/st.mcu019-Dbg.cmd
@@ -2,32 +2,44 @@ require EthercatMC,USER
 
 epicsEnvSet("MOTOR_PORT",    "$(SM_MOTOR_PORT=MCU1)")
 
-epicsEnvSet("IPADDR",        "$(SM_IPADDR=127.0.0.1)")
+epicsEnvSet("IPADDR",        "$(SM_IPADDR=10.4.3.205)")
 epicsEnvSet("IPPORT",        "$(SM_IPPORT=5000)")
 epicsEnvSet("ASYN_PORT",     "$(SM_ASYN_PORT=MC_CPU1)")
-epicsEnvSet("PREFIX",        "$(SM_PREFIX=IOC:)")
-epicsEnvSet("MOTOR_NAME",    "$(SM_MOTOR_NAME=m1)")
-epicsEnvSet("AXIS_NO",       "$(SM_AXIS_NO=1)")
-epicsEnvSet("DESC",          "$(SM_DESC=Lower=Right)")
+epicsEnvSet("PREFIX",        "$(SM_PREFIX=LabS-ESSIIP:)")
+epicsEnvSet("P",             "$(SM_PREFIX=LabS-ESSIIP:)")
 epicsEnvSet("EGU",           "$(SM_EGU=mm)")
 epicsEnvSet("PREC",          "$(SM_PREC=3)")
 
-# And this reads the input of an incremental encoder terminal
-# on the EtherCAT bus. Works with the simulator.
-# For real terminals the adresses must be adapted
-epicsEnvSet("AXISCONFIG",    "stepSize=1.0;HomProc=2;HomPos=179;encoder=ADSPORT=501/.ADR.16#3040010,16#80000049,2,2")
-
+# Controller
 < EthercatMCController.cmd
+
+
+# Axis 1
+epicsEnvSet("MOTOR_NAME",    "$(SM_MOTOR_NAME=MC-MCU-02:m1)")
+epicsEnvSet("AXIS_NO",       "$(SM_AXIS_NO=1)")
+epicsEnvSet("DESC",          "$(SM_DESC=Lower=Right)")
+epicsEnvSet("AXISCONFIG",    "HomProc=1;HomPos=-63;encoder=ADSPORT=501/.ADR.16#3040010,16#80000049,2,2")
+
+
 < EthercatMCAxis.cmd
 < EthercatMCAxisdebug.cmd
 < EthercatMCAxishome.cmd
 
-epicsEnvSet("AXISCONFIG",    "stepSize=1.0;HomProc=1;HomPos=0;encoder=ADSPORT=501/.ADR.16#3040010,16#8000004F,2,2")
 
-epicsEnvSet("MOTOR_NAME",    "$(SM_MOTOR_NAME=m2)")
+epicsEnvSet("MOTOR_NAME",    "$(SM_MOTOR_NAME=MC-MCU-02:m2)")
 epicsEnvSet("AXIS_NO",       "$(SM_AXIS_NO=2)")
 epicsEnvSet("DESC",          "$(SM_DESC=Upper=Left)")
+epicsEnvSet("AXISCONFIG",    "HomProc=2;HomPos=64;encoder=ADSPORT=501/.ADR.16#3040010,16#8000004F,2,2")
+
 < EthercatMCAxis.cmd
 < EthercatMCAxisdebug.cmd
 < EthercatMCAxishome.cmd
 
+## Logical axes and slit
+epicsEnvSet("SLIT",          "$(SM_SLIT=MC-SLT-01:SltH-)")
+epicsEnvSet("mXp",           "$(SM_mXp=MC-MCU-02:m2)")
+epicsEnvSet("mXn",           "$(SM_mXp=MC-MCU-02:m1)")
+
+## Slit
+< EthercatMC2slit.cmd
+#########################
