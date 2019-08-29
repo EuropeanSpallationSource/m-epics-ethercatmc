9cc6a1d445d85278e4499d43e791e39b3cd511d3
Torsten Bögershausen <torsten.bogershausen@esss.se>
2019-08-29 17:03:25 +0200

    TC 132: Fallback to VELO/2 when JVEL is 0
    When JVEL is 0, TC 132 fails.
    The indexer (as today) does not have JVEL, so fall back to
    VELO / 2.0 in this case.

--- test/nose_tests/132_Record-JOGR_LLS.py
+++ test/nose_tests/132_Record-JOGR_LLS.py
@@ -21,12 +21,17 @@ class Test(unittest.TestCase):
     per30_UserPosition  = round((7 * llm + 3 * hlm) / 10)
 
     range_postion    = hlm - llm
-    jogging_velocity = epics.caget(motor + '.JVEL')
+    saved_JVEL       = epics.caget(motor + '.JVEL')
     moving_velocity  = epics.caget(motor + '.VELO')
     acceleration     = epics.caget(motor + '.ACCL')
     msta             = int(epics.caget(motor + '.MSTA'))
+    if (saved_JVEL != 0.0):
+        jogging_velocity = saved_JVEL;
+    else:
+        jogging_velocity = moving_velocity / 2.0;
 
     print 'llm=%f hlm=%f per30_UserPosition=%f' % (llm, hlm, per30_UserPosition)
+    print 'saved_JVEL=%f jogging_velocity=%f' % (saved_JVEL, jogging_velocity)
 
     # Assert if motor is homed
     def test_TC_1321(self):
@@ -34,13 +39,13 @@ class Test(unittest.TestCase):
         tc_no = "TC-1321"
         self.assertNotEqual(0, self.msta & self.lib.MSTA_BIT_HOMED, 'MSTA.homed (Axis has been homed)')
         self.assertNotEqual(self.llm, self.hlm, 'llm must be != hlm')
-
+        self.assertNotEqual(0, self.jogging_velocity, 'JVEL or VELO must be != 0.0')
 
 
     # high limit switch
     def test_TC_1322(self):
         motor = self.motor
-        if (self.msta & self.lib.MSTA_BIT_HOMED):
+        if (self.msta & self.lib.MSTA_BIT_HOMED) and (self.jogging_velocity != 0.0):
             tc_no = "TC-1322-low-limit-switch"
             print '%s' % tc_no
             old_high_limit = epics.caget(motor + '.HLM')
@@ -52,6 +57,8 @@ class Test(unittest.TestCase):
             # low or high must be set to 0 first
             self.lib.setSoftLimitsOff(motor)
 
+            if (self.saved_JVEL == 0.0) :
+                epics.caput(motor + '.JVEL', self.jogging_velocity)
             epics.caput(motor + '.JOGR', 1, wait=True)
             # Get values, check them later
             lvio = int(epics.caget(motor + '.LVIO'))
@@ -61,6 +68,7 @@ class Test(unittest.TestCase):
             print '%s msta=%x lvio=%d' % (tc_no, mstaE, lvio)
 
             self.lib.setSoftLimitsOn(motor, old_low_limit, old_high_limit)
+            epics.caput(motor + '.JVEL', self.saved_JVEL)
 
             #self.assertEqual(0, lvio, 'LVIO == 0')
             self.assertEqual(0, mstaE & self.lib.MSTA_BIT_PROBLEM,    'No Error MSTA.Problem at PLUS_LS')
