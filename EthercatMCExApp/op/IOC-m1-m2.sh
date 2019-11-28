a0eb73e44be5d391236dc496ea1dfb70bb8e4e2f
Torsten Bögershausen <torsten.bogershausen@esss.se>
2019-11-28 08:49:33 +0100

    op/IOC-m1-m2.sh: try to find medm in working tree
    If medm can not be found in the $PATH, see if there is a binary
    in the working tree (under epics/extensions/...)

--- EthercatMCExApp/op/IOC-m1-m2.sh
+++ EthercatMCExApp/op/IOC-m1-m2.sh
@@ -1,9 +1,16 @@
 #!/bin/sh
 
 ADLFILE=adl/IOC-m1-m2.adl
-
+MEDM=medm
+if ! type $MEDM; then
+    FILE=../../../../extensions/src/medm/medm/O.$EPICS_HOST_ARCH/medm
+    if test -x $FILE; then
+	MEDM=$FILE
+    fi
+fi
+export MEDM
 rm -f $ADLFILE &&
 ( cd adl/tools && make ) &&
 cp -u adl/motor-2.adl $ADLFILE
 
-medm -x -macro "P=IOC:,M1=m1,M2=m2" $ADLFILE
+$MEDM -x -macro "P=IOC:,M1=m1,M2=m2" $ADLFILE
