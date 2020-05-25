8558668444b3ba9034020a33e9baa786cb9cd22f
Torsten Bögershausen <torsten.bogershausen@esss.se>
2020-05-25 08:22:33 +0200

    install-pytest-pyepics-p4p.sh: Install packages even without sudo
    Try to install python packages without sudo:
    Hoepfully they will end up in the virtualenv we are running under travis

--- test/install-pytest-pyepics-p4p.sh
+++ test/install-pytest-pyepics-p4p.sh
@@ -5,6 +5,10 @@ sudo pip install pytest
 sudo pip install pyepics
 sudo pip install p4p
 
+pip install pytest
+pip install pyepics
+pip install p4p
+
 sudo apt-get install pip3 || :
 sudo pip3 install pytest || :
 sudo pip3 install pyepics || :
