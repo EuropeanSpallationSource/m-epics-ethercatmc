c687808d173a2f2f1f1503b8f1b5618a1e095af5
Torsten Bögershausen <torsten.bogershausen@esss.se>
2020-06-17 08:07:11 +0200

    Uodates for gitlab-ci
    Updating the ci-scripts broke the gitlab-ci builds:
    - Change .compile-epics.sh to run the python-scripts from .ci/
    - Remove the installation of bash (since we use python)
    - Update .ci to not bail out on a not-defined TRAVIS_COMPILER
      (The last one should be fixable by setting an environment variable,
       but that doesn't work as expected. Investigations are ongoing)

--- .compile-epics.sh
+++ .compile-epics.sh
@@ -29,29 +29,6 @@ then
   export MOTOR_RECURSIVE=NO
 fi
 
-if [ "$BASH_VERSINFO" -lt 4 ]
-then
-  if test -x /opt/local/bin/port; then
-    # Mac ports
-    if ! [ -x /opt/local/bin/bash ]
-    then
-      sudo port install bash
-    fi
-    exec /opt/local/bin/bash $0 "$@"
-    APTGET="$SUDO port install"
-  fi
-  # brew (Mac)
-  if test -x /usr/local/bin/brew; then
-    if ! [ -x /usr/local/bin/bash ]
-    then
-      sudo brew install bash
-    fi
-    exec /usr/local/bin/bash $0 "$@"
-  fi
-  echo >&2 "Can not find bash version "
-  echo >&2 "Unable to install bash version via port (mac ports)"
-  echo >&2 "Unable to install bash version via brew"
-  exit 1
-fi
+env
 
-bash  -e -x .ci/travis/prepare.sh -v -v
+python .ci/cue.py prepare
