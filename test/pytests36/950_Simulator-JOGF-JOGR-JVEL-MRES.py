1c56b020af89050060d5a6e33c17a6b4c7204a91
Torsten Bögershausen <torsten.bogershausen@esss.se>
2020-07-10 15:22:01 +0200

    test: Add 950_Simulator-JOGF-JOGR-JVEL-MRES.py
    Add asimulator test for a feature in the motorRecord:
    Start "jogging" via JOGF or JOGR, and change JVEL.
    The motorRecord issued us the wrong velocity.

---
+++ test/pytests36/950_Simulator-JOGF-JOGR-JVEL-MRES.py
@@ -0,0 +1,433 @@
+#!/usr/bin/env python
+#
+
+import math
+import os
+import sys
+import unittest
+from AxisMr import AxisMr
+from AxisCom import AxisCom
+
+import time
+import math
+import inspect
+
+###
+
+#
+# Test cases to check if JVEL and JAR is send correct to the controller
+# The readout is done with additional Records, which already
+# exist in the (old) ethercatmcAxis driver.
+# This driver (and the corresponding MCU software) allow
+# the readout of the commanded velocity and acceleration
+# for both "(EPICS) jogging" and positioning.
+#
+#
+
+
+def lineno():
+    return inspect.currentframe().f_back.f_lineno
+
+
+polltime = 0.1
+
+# Values to be used for test
+# Note: Make sure to use different values to hae a good
+# test coverage
+myVELO = 5.0  # positioning velocity
+myACCL = 1.0  # Time to VELO, seconds
+
+# Different values, high use even, low uses odd
+#
+myLowHardLimitPos = -19.0
+myDLLM = 0.0
+myStartposDial = 0.0
+myDHLM = 0.0
+myHighHardLimitPos = 18.0
+
+# Comparing floating points may fail because of rounding problems
+maxdelta = 0.01
+
+# We need to test different combinations of
+# - MRES > 0       ; MRES < 0
+# DIR=0; OFF=X     ; DIR=1; OFF=X
+# (Those above are the main loop)
+#
+#
+# Controller with and without "read only limits"
+# Write to DHLM, DLLM, HLM, LLM
+
+
+def InitAllFor950(self, tc_no):
+    # msta = int(self.axisCom.get(".MSTA"))
+    # assert msta & self.axisMr.MSTA_BIT_HOMED  # , 'MSTA.homed (Axis has been homed)')
+
+    # Prepare parameters for jogging and backlash
+    self.axisCom.put(".VELO", myVELO)
+    self.axisCom.put(".ACCL", myACCL)
+
+    self.axisCom.put(".BDST", 0.0)
+    self.axisMr.setValueOnSimulator(tc_no, "bAxisHomed", 1)
+    self.axisMr.setValueOnSimulator(tc_no, "nAmplifierPercent", 100)
+    self.axisMr.setValueOnSimulator(tc_no, "fHighHardLimitPos", myHighHardLimitPos)
+    self.axisMr.setValueOnSimulator(tc_no, "fHighSoftLimitPos", myHighHardLimitPos)
+    self.axisMr.setValueOnSimulator(tc_no, "fLowHardLimitPos", myLowHardLimitPos)
+    self.axisMr.setValueOnSimulator(tc_no, "fLowSoftLimitPos", myLowHardLimitPos)
+
+    # Move the  to 0, to avoid limit switch activation
+    self.axisMr.setValueOnSimulator(tc_no, "fActPosition", myStartposDial)
+    self.axisCom.put(".DVAL", myStartposDial)
+    # Speed it up, by setting the position in the simulator
+    # and wait for the movement to finish
+    self.axisMr.waitForStop(tc_no, 2.0)
+
+
+def InitLimitsNoROlimits(self, tc_no):
+    self.axisCom.put("-CfgDLLM-En", 0, wait=True)
+    self.axisCom.put("-CfgDHLM-En", 0, wait=True)
+
+    ## XXX self.axisCom.put("-DbgStrToLOG", "initLim " + str(tc_no)[0:20])
+    maxTime = 5  # 5 seconds maximum to let read only parameters ripple through
+    maxDelta = 0.05  # 5 % error tolerance margin
+    while maxTime > 0:
+        self.axisCom.put(".DHLM", myDHLM)
+        self.axisCom.put(".DLLM", myDLLM)
+
+        actDHLM = self.axisCom.get(".DHLM")
+        actDLLM = self.axisCom.get(".DLLM")
+
+        print(
+            "%s:%d expDHLM=%f actDHLM=%f expDLLM=%f actDLLM=%f"
+            % (tc_no, lineno(), myDHLM, actDHLM, myDLLM, actDLLM)
+        )
+
+        resH = self.axisMr.calcAlmostEqual(tc_no, myDHLM, actDHLM, maxDelta)
+        resL = self.axisMr.calcAlmostEqual(tc_no, myDLLM, actDLLM, maxDelta)
+        print(f"{tc_no}:{int(lineno())} resH={resH} resL={resL}")
+        if (resH == True) and (resL == True):
+            return True
+
+        time.sleep(polltime)
+        maxTime = maxTime - polltime
+    return False
+
+
+def jogTheMotorToLS(
+    self,
+    tc_no,
+    mres=0,
+    dir=-1,
+    jogX=0,
+    jvel=0,
+    jar=0,
+    drvUseEGU=0,
+    velRB=0,
+    accRB=0,
+    dhls=-1,
+    dlls=-1,
+    hls=-1,
+    lls=-1,
+):
+    self.axisCom.put("-DbgStrToLOG", "Start " + str(tc_no))
+    self.assertNotEqual(0, mres, str(tc_no) + "mres must not be 0")
+    self.assertNotEqual(-1, dir, str(tc_no) + "dir must not be -1")
+    self.assertNotEqual(0, jogX, str(tc_no) + "jogx must not be 0")
+    self.assertNotEqual(0, jvel, str(tc_no) + "jvel must not be 0")
+    self.assertNotEqual(0, jar, str(tc_no) + "jar must not be 0")
+    self.assertNotEqual(0, velRB, str(tc_no) + "velRB must not be 0")
+    self.assertNotEqual(0, accRB, str(tc_no) + "accRB must not be 0")
+    self.assertNotEqual(-1, dhls, str(tc_no) + "dhls must not be -1")
+    self.assertNotEqual(-1, dlls, str(tc_no) + "dlls must not be -1")
+    self.assertNotEqual(-1, hls, str(tc_no) + "hls must not be -1")
+    self.assertNotEqual(-1, lls, str(tc_no) + "lls must not be -1")
+    self.axisCom.put("." + jogX, 0)
+    # Always start in the middle
+    destination = 0.0
+    self.axisMr.moveWait(tc_no, destination)
+    # wait_for_stop = 30
+    # self.axisMr.waitForStop(tc_no, wait_for_stop)
+    mresAct = self.axisCom.get(".MRES")
+    dirAct = self.axisCom.get(".DIR")
+    if (mresAct != mres) or (dirAct != dir):
+        self.axisCom.put(".MRES", mres)
+        self.axisCom.put(".DIR", dir)
+
+    time.sleep(1.0)
+    self.axisCom.put(".JVEL", jvel)
+    self.axisCom.put(".JAR", jar)
+    self.axisCom.put("." + jogX, 1)
+    time.sleep(2.0)
+    wait_for_done = 60
+    self.axisMr.waitForStartAndDone(str(tc_no) + "B", wait_for_done)
+
+    actHLS = self.axisCom.get(".HLS")
+    actLLS = self.axisCom.get(".LLS")
+    actVelRB = self.axisCom.get("-Vel-RB")
+    actAccRB = self.axisCom.get("-Acc-RB")
+    msta = int(self.axisCom.get(".MSTA"))
+    if msta & self.axisMr.MSTA_BIT_PLUS_LS:
+        actDHLS = 1
+    else:
+        actDHLS = 0
+    if msta & self.axisMr.MSTA_BIT_MINUS_LS:
+        actDLLS = 1
+    else:
+        actDLLS = 0
+
+    print(f"tc_no={tc_no} mres={mres} dir={dir} drvUseEGU={drvUseEGU}")
+    print(
+        f"tc_no={tc_no} expVelRB={velRB} expAccRB={accRB} expHLS={hls} expLLS={lls} expDHLS={dhls} expDLLS={dlls}"
+    )
+    print(
+        f"tc_no={tc_no} actVelRB={actVelRB} actAccRB={actAccRB} actHLS={actHLS} actLLS={actLLS} actDHLS={actDHLS} actDLLS={actDLLS}"
+    )
+    maxdelta = 0.1
+    okVelRB = self.axisMr.calcAlmostEqual(
+        str(tc_no) + " actVelRB", velRB, actVelRB, maxdelta
+    )
+    okAccRB = self.axisMr.calcAlmostEqual(
+        str(tc_no) + " actAccRB", accRB, actAccRB, maxdelta
+    )
+    testPassed = (
+        okVelRB
+        and okAccRB
+        and (hls == actHLS)
+        and (lls == actLLS)
+        and (dhls == actDHLS)
+        and (dlls == actDLLS)
+    )
+    if testPassed:
+        self.axisCom.put("-DbgStrToLOG", "Passed " + str(tc_no))
+    else:
+        self.axisCom.put("-DbgStrToLOG", "Failed " + str(tc_no))
+    assert testPassed
+
+
+def jogTheMotorChangeJVEL(
+    self, tc_no, jogX=0, jvel=0, jar=0, velRB=0, accRB=0,
+):
+    self.axisCom.put("-DbgStrToLOG", "Start " + str(tc_no))
+    self.assertNotEqual(0, jogX, str(tc_no) + "jogx must not be 0")
+    self.assertNotEqual(0, jvel, str(tc_no) + "jvel must not be 0")
+    self.assertNotEqual(0, jar, str(tc_no) + "jar must not be 0")
+    self.assertNotEqual(0, velRB, str(tc_no) + "velRB must not be 0")
+    self.assertNotEqual(0, accRB, str(tc_no) + "accRB must not be 0")
+    # Start jogging
+    self.axisCom.put("." + jogX, 1)
+    time.sleep(2.0)
+    wait_for_start = 2
+    self.axisMr.waitForStart(tc_no, wait_for_start)
+
+    # change JVEL while jogging
+    self.axisCom.put(".JVEL", jvel)
+    maxdelta = 0.1
+    maxTime = 5
+    while maxTime > 0:
+        actVelRB = self.axisCom.get("-Vel-RB")
+        actAccRB = self.axisCom.get("-Acc-RB")
+
+        okVelRB = self.axisMr.calcAlmostEqual(
+            str(tc_no) + "actVelRB", velRB, actVelRB, maxdelta
+        )
+        okAccRB = self.axisMr.calcAlmostEqual(
+            str(tc_no) + "actAccRB", accRB, actAccRB, maxdelta
+        )
+        testPassed = okVelRB and okAccRB
+        if testPassed:
+            maxTime = 0
+        else:
+            time.sleep(polltime)
+            maxTime = maxTime - polltime
+    print(f"tc_no={tc_no} expVelRB={velRB} expAccRB={accRB}")
+    print(f"tc_no={tc_no} actVelRB={actVelRB} actAccRB={actAccRB}")
+    self.axisCom.put("." + jogX, 0)
+    self.axisCom.put(".STOP", 1)
+    wait_for_stop = 5
+    self.axisMr.waitForStop(tc_no, wait_for_stop)
+
+    if testPassed:
+        self.axisCom.put("-DbgStrToLOG", "Passed " + str(tc_no))
+    else:
+        self.axisCom.put("-DbgStrToLOG", "Failed " + str(tc_no))
+    assert testPassed
+
+
+def jogTheMotorWithMRES(
+    self, tc_no, mres=0, dir=-1, jogDir=-1, jvel=0, jar=0, drvUseEGU=0
+):
+    self.assertNotEqual(0, mres, str(tc_no) + "mres must not be 0")
+    self.assertNotEqual(-1, dir, str(tc_no) + "dir must not be -1")
+    self.assertNotEqual(-1, jogDir, str(tc_no) + "jogDir must not be -1")
+    self.assertNotEqual(0, jvel, str(tc_no) + "jvel must not be 0")
+    self.assertNotEqual(0, jar, str(tc_no) + "jar must not be 0")
+
+    # Command to the motorRecord
+    if jogDir == 1:
+        jogX = "JOGF"
+        hls = 1
+        lls = 0
+    else:
+        self.assertEqual(0, jogDir, str(tc_no) + "jogDir must be 0 or 1")
+        jogX = "JOGR"
+        hls = 0
+        lls = 1
+
+    # The motorRecord User LS (hls/lls) follow the jogging direction
+    hls = jogDir
+    lls = 1 - jogDir
+    # The "mechanical LS" are depending on MRES and DIR
+    dhls = hls
+    dlls = lls
+
+    if drvUseEGU == 0:
+        # Classic motorRecord
+        velRB = jvel / mres
+        accRB = jar / math.fabs(mres)
+    else:
+        # motorRecord using EGU
+        velRB = jvel * math.fabs(mres) / mres  # Keep the sign
+        accRB = jar
+
+    if mres < 0:
+        dhls = 1 - dhls
+        dlls = 1 - dlls
+    if dir > 0:
+        # dir == 1 means "invers dial <--> user direction
+        dhls = 1 - dhls
+        dlls = 1 - dlls
+        velRB = 0 - velRB  # Change sign
+
+    # Now we have calulated all data
+    jogTheMotorToLS(
+        self,
+        tc_no,
+        mres=mres,
+        dir=dir,
+        jogX=jogX,
+        jvel=jvel,
+        jar=jar,
+        drvUseEGU=drvUseEGU,
+        velRB=velRB,
+        accRB=accRB,
+        dhls=dhls,
+        dlls=dlls,
+        hls=hls,
+        lls=lls,
+    )
+    # 2nd round, jog away from LS and, once moving,
+    # change JVEL
+    tc_no = tc_no + 1
+    jvel2 = jvel / 2.0
+    velRB2 = (0 - velRB) / 2.0
+    accRB2 = accRB
+    if jogDir == 1:
+        jogX2 = "JOGR"
+    else:
+        self.assertEqual(0, jogDir, str(tc_no) + "jogDir must be 0 or 1")
+        jogX2 = "JOGF"
+    jogTheMotorChangeJVEL(
+        self, tc_no, jogX=jogX2, jvel=jvel2, jar=jar, velRB=velRB2, accRB=accRB2,
+    )
+
+
+def jogTheMotorTestWrapper(self, tc_no, mres=0, dir=-1, jogDir=-1, jvel=0, jar=0):
+    # Check which motorRecord version we have
+    vers = float(self.axisCom.get(".VERS"))
+    if vers >= 6.94 and vers <= 7.09:
+        # Special feature: The motorRecord can talk to the driver
+        # in EGU. Changing MRES does NOT affect velocity
+        hasDrvUseEGUfeature = True
+        self.axisCom.put("-DrvUseEGU", 0)
+    else:
+        hasDrvUseEGUfeature = False
+
+    jogTheMotorWithMRES(
+        self, tc_no, mres=mres, dir=dir, jogDir=jogDir, jvel=jvel, jar=jar
+    )
+
+    # Check if mres changes the raw velocity/acceleration
+    tc_no = tc_no + 2
+    mres2 = mres * 2
+    jogTheMotorWithMRES(
+        self, tc_no, mres=mres2, dir=dir, jogDir=jogDir, jvel=jvel, jar=jar
+    )
+    if hasDrvUseEGUfeature:
+        drvUseEGU = 1
+        self.axisCom.put("-DrvUseEGU", drvUseEGU)
+        tc_no = tc_no + 2
+        mres3 = mres * 3
+        jogTheMotorWithMRES(
+            self,
+            tc_no,
+            mres=mres3,
+            dir=dir,
+            jogDir=jogDir,
+            jvel=jvel,
+            jar=jar,
+            drvUseEGU=drvUseEGU,
+        )
+
+
+class Test(unittest.TestCase):
+    drvUseEGU_RB = None
+    drvUseEGU = 0
+    url_string = os.getenv("TESTEDMOTORAXIS")
+    print(f"url_string={url_string}")
+
+    axisCom = AxisCom(url_string, log_debug=False)
+    axisMr = AxisMr(axisCom)
+
+    def test_TC_95000(self):
+        tc_no = 95000
+        self.axisCom.put("-DbgStrToLOG", "Start " + str(tc_no))
+        InitAllFor950(self, tc_no)
+        InitLimitsNoROlimits(self, tc_no)
+        self.axisCom.put("-DbgStrToLOG", "End " + str(tc_no))
+
+    def test_TC_95010(self):
+        tc_no = 95010
+        mres = 1.0
+        dir = 0
+        jogDir = 1
+        jvel = 2.0
+        jar = 3.0
+
+        jogTheMotorTestWrapper(
+            self, tc_no, mres=mres, dir=dir, jogDir=jogDir, jvel=jvel, jar=jar
+        )
+
+    def test_TC_95020(self):
+        tc_no = 95020
+        mres = 1.0
+        dir = 1
+        jogDir = 1
+        jvel = 2.0
+        jar = 3.0
+
+        jogTheMotorTestWrapper(
+            self, tc_no, mres=mres, dir=dir, jogDir=jogDir, jvel=jvel, jar=jar
+        )
+
+    def test_TC_95030(self):
+        tc_no = 95030
+        mres = -1.0
+        dir = 1
+        jogDir = 1
+        jvel = 2.0
+        jar = 3.0
+
+        jogTheMotorTestWrapper(
+            self, tc_no, mres=mres, dir=dir, jogDir=jogDir, jvel=jvel, jar=jar
+        )
+
+    def test_TC_95040(self):
+        tc_no = 95040
+        mres = -1.0
+        dir = 0
+        jogDir = 1
+        jvel = 2.0
+        jar = 3.0
+
+        jogTheMotorTestWrapper(
+            self, tc_no, mres=mres, dir=dir, jogDir=jogDir, jvel=jvel, jar=jar
+        )
