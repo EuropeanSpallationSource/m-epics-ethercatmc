2aeddd392e31ab53318214cc4d78402504224620
Torsten Bögershausen <torsten.bogershausen@esss.se>
2020-12-18 09:00:28 +0100

    test/simulator: Expose the motorHandleADS_ADR_xx() functions
    Make the motorHandleADS_ADR_xx() from cmd_EAT.c public.
    They will be used in the next commit, when ads.c learns to use them

--- test/simulator/cmd_EAT.h
+++ test/simulator/cmd_EAT.h
@@ -1,1 +1,25 @@
+#ifndef CMD_EAT_H
+#define CMD_EAT_H
+
 void cmd_EAT(int argc, const char *argv[]);
+int motorHandleADS_ADR_getInt(unsigned adsport,
+                              unsigned indexGroup,
+                              unsigned indexOffset,
+                              int *iValue);
+
+int motorHandleADS_ADR_putInt(unsigned adsport,
+                              unsigned indexGroup,
+                              unsigned indexOffset,
+                              int iValue);
+
+int motorHandleADS_ADR_getFloat(unsigned adsport,
+                                unsigned indexGroup,
+                                unsigned indexOffset,
+                                double *fValue);
+
+int motorHandleADS_ADR_putFloat(unsigned adsport,
+                                unsigned indexGroup,
+                                unsigned indexOffset,
+                                double fValue);
+
+#endif
