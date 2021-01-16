a5eaa97ef4a417c92abc8108c2e794018a4cb0b3
Torsten Bögershausen <torsten.bogershausen@esss.se>
2021-01-16 18:48:43 +0100

    test/install-pytest-pyepics-p4p.sh: Remove old pip
    Remove all calls to pip, we can/must use pip3.
    Tested under Ubuntu 20.04
    May need adjustments for other Linux versions ??

--- test/install-pytest-pyepics-p4p.sh
+++ test/install-pytest-pyepics-p4p.sh
@@ -1,15 +1,8 @@
 #!/bin/sh
 set -e -x
 
-sudo pip install pytest
-sudo pip install pyepics
-sudo pip install p4p
-
-pip install pytest
-pip install pyepics
-pip install p4p
-
 sudo apt-get install pip3 || :
+sudo apt-get install python3-pip || :
 sudo pip3 install pytest || :
 sudo pip3 install pyepics || :
 sudo pip3 install p4p || :
