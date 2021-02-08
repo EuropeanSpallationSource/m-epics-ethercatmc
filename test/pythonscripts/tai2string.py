6196a2de68c662452c9318338097f92bc5479297
Torsten Bögershausen <torsten.bogershausen@esss.se>
2021-02-08 09:27:24 +0000

    test/pythonscripts/tai2string.py: Make more readable
    Rename variables to make stuff more readable

--- test/pythonscripts/tai2string.py
+++ test/pythonscripts/tai2string.py
@@ -18,35 +18,35 @@ def main(argv=None):
         argv = sys.argv
 
 #LabS-MCAG:MC-MCU-07:nSystemTAIclock 2021-02-03 07:52:39.632  1612338796622117100
-    RE_MATCH_SYSTEMTAICLOCK = re.compile(
+    RE_MATCH_TAI = re.compile(
         #r"(\S*TAIclock\S*)\s+(\d+-\d*-\d*\s\d+:\d+:\d\.\d+)"
         r"(\S*TAI\S*)(\s+)(\d+-\d*-\d*\s+\d+:\d+:\d+\.\d+)(\s+)(\d+)$"
     )
 
     for line in sys.stdin:
         line = line.strip()
-        match_systemtaiclock = RE_MATCH_SYSTEMTAICLOCK.match(line)
-        if match_systemtaiclock != None:
+        match_tai = RE_MATCH_TAI.match(line)
+        if match_tai != None:
             gidx = 1
-            pvname = match_systemtaiclock.group(gidx)
+            pvname = match_tai.group(gidx)
             gidx = gidx + 1
-            sep1 = match_systemtaiclock.group(gidx)
+            sep1 = match_tai.group(gidx)
             gidx = gidx + 1
-            datetime = match_systemtaiclock.group(gidx)
+            datetime = match_tai.group(gidx)
             gidx = gidx + 1
-            sep2 = match_systemtaiclock.group(gidx)
+            sep2 = match_tai.group(gidx)
             gidx = gidx + 1
-            allnanoseconds = int(match_systemtaiclock.group(gidx))
+            allnsec_plc_tai = int(match_tai.group(gidx))
             gidx = gidx + 1
             #print (f"{line} match_systemtaicloc")
-            #print (f"pvname={pvname} datetime={datetime} allnanoseconds={allnanoseconds}")
-            seconds = int(allnanoseconds / 1000000000)
-            subnsec = allnanoseconds % 1000000000
-            alldatetime=time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime(seconds))
-            epoch_or_so = int(time.time())
-            #print(f"pvname={pvname} datetime={datetime} allnanoseconds={allnanoseconds} alldatetime={alldatetime}")
-            #print(f"{pvname}{sep1}{datetime}{sep2}{alldatetime}.{subnsec:09d} {allnanoseconds} {epoch_or_so} {seconds}")
-            print(f"{pvname}{sep1}{datetime}{sep2}{alldatetime}.{subnsec:09d} epoch={epoch_or_so} tai={seconds}")
+            #print (f"pvname={pvname} datetime={datetime} allnsec_plc_tai={allnsec_plc_tai}")
+            sec_tai_plc = int(allnsec_plc_tai / 1000000000)
+            subnsec = allnsec_plc_tai % 1000000000
+            alldatetime=time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime(sec_tai_plc))
+            epoch_now = int(time.time())
+            #print(f"pvname={pvname} datetime={datetime} allnsec_plc_tai={allnsec_plc_tai} alldatetime={alldatetime}")
+            #print(f"{pvname}{sep1}{datetime}{sep2}{alldatetime}.{subnsec:09d} {allnsec_plc_tai} {epoch_now} {sec_tai_plc}")
+            print(f"{pvname}{sep1}{datetime}{sep2}{alldatetime}.{subnsec:09d} epoch-now={epoch_now} sec_tai_plc={sec_tai_plc}")
         else:
             print (line)
 
