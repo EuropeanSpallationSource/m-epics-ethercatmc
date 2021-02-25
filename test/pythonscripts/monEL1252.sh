5a772c53fb0c1e0eebb6ba57cb70dddb3cfb86c5
Torsten Bögershausen <torsten.bogershausen@esss.se>
2021-02-25 13:52:06 +0000

    monEL1252.sh: Use P macro for PV names

--- test/pythonscripts/monEL1252.sh
+++ test/pythonscripts/monEL1252.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-
+P="LabS-MCAG:MC-MCU-07:"
 if ! type pvmonitor >/dev/null ; then
   . ~/MCAG_setupMotionDemo.191003-base-7.0.3/.epics.ics-vm-mc-ioc-01.cslab.esss.lu.se.Linux.x86_64
 fi
@@ -9,4 +9,7 @@ fi
 
 #pvmonitor LabS-MCAG:MC-MCU-07:PTPState  LabS-MCAG:MC-MCU-07:sSystemTAIclock LabS-MCAG:MC-MCU-07:nSystemTAIclock LabS-MCAG:MC-MCU-07:TAIEL1252N0
 pvmonitor LabS-MCAG:MC-MCU-07:PTPState  LabS-MCAG:MC-MCU-07:UTCEL1252P0 LabS-MCAG:MC-MCU-07:UTCEL1252N0| ./tai2string.py
+pvmonitor ${P}PTPState ${P}PTPOffset  ${P}UTCEL1252P0 ${P}DcToExtTimeOffsetEL6688 ${P}DcToExtTimeOffsetSystem     ${P}WriteDoneExtTimeOffset | ./tai2string.py  2>&1 | tee EL1252.txt
+
+
 
