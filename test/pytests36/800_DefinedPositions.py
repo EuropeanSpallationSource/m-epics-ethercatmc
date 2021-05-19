43e6be2d25703065d5a56eecf90b1c5ac3a97930
Torsten Bögershausen <torsten.bogershausen@esss.se>
2021-05-19 07:07:39 +0200

    test/800: Inclrease timeout and cleanup
    Increase the timeouts:
    The acceleration is not always reported from the controller.
    
    Remove the debug:
    Don't change TPRO or SPAM fields.
    
    And don't change the .DOL and .OMSL fields any more:
    The record do this for us

--- test/pytests36/800_DefinedPositions.py
+++ test/pytests36/800_DefinedPositions.py
@@ -13,18 +13,6 @@ tc_no_base = int(os.path.basename(__file__)[0:3]) * 10
 ###
 
 
-def setTPRO(self, tpro):
-    self.axisCom.put("-DefPosSEL.TPRO", tpro)
-    self.axisCom.put("-DefPosSELCALC1.TPRO", tpro)
-    self.axisCom.put("-DefPosSELCALC2.TPRO", tpro)
-    self.axisCom.put("-DefPosSetDOL_.TPRO", tpro)
-    self.axisCom.put("-DefPosSEL1.TPRO", tpro)
-    if tpro == 1:
-        self.axisCom.put(".SPAM", 2047)
-    else:
-        self.axisCom.put(".SPAM", 15)
-
-
 class Test(unittest.TestCase):
     url_string = os.getenv("TESTEDMOTORAXIS")
     print(
@@ -62,12 +50,7 @@ class Test(unittest.TestCase):
     def test_TC_8002(self):
         msta = int(self.axisCom.get(".MSTA"))
         if msta & self.axisMr.MSTA_BIT_HOMED:
-            old_dol = self.axisCom.get(".DOL")
-            old_omsl = self.axisCom.get(".OMSL")
-            setTPRO(self, 1)
             # Note: Setting up .DOL and .OMLS is done in the template
-            # self.axisCom.put(".DOL", self.motorPvName + "-DefPosSEL1")
-            # self.axisCom.put(".OMSL", "closed_loop")
             velo = self.axisCom.get(".VELO")
 
             # Calculate values
@@ -83,16 +66,18 @@ class Test(unittest.TestCase):
                     float(step) * self.hlm + float(steps - step) * self.llm
                 ) / float(steps - 1)
                 self.axisCom.put(pvname, destination)
-                timeout = self.axisMr.calcTimeOut(destination, velo)
-                self.axisCom.put("-DefPosSEL", step, wait=True, timeout=timeout)
-                # The following should not be needed:
+                # Workaround: Acceleration may be wrong Allow longer timeout
+                timeout = 2 + 2 * self.axisMr.calcTimeOut(destination, velo)
+                # The -DefPosSEL does not wait on a callback
+                # self.axisCom.put("-DefPosSEL", step, wait=True, timeout=timeout)
+                self.axisCom.put("-DefPosSEL", step)
+                # The following is needed (because there is no callback)
                 if not self.axisMr.verifyRBVinsideRDBD(tc_no, destination):
                     try:
                         self.axisMr.waitForStart(tc_no, 1.0)
                     except Exception:
                         pass
-
-                # self.axisMr.waitForStop(tc_no, timeout)
+                self.axisMr.waitForStop(tc_no, timeout)
 
                 postMoveCheckOK = self.axisMr.postMoveCheck(tc_no)
                 verifyRBVinsideRDBDOK = self.axisMr.verifyRBVinsideRDBD(
@@ -108,7 +93,7 @@ class Test(unittest.TestCase):
                     self.axisCom.put("-DbgStrToLOG", "Passed " + str(tc_no), wait=True)
                 else:
                     self.axisCom.put("-DbgStrToLOG", "Failed " + str(tc_no), wait=True)
-            self.axisCom.put(".DOL", old_dol)
-            self.axisCom.put(".OMSL", old_omsl)
-            setTPRO(self, 0)
+
+            self.axisCom.put("-DefPosSEL", -1)
+
             assert passed
