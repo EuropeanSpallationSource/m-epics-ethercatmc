868b6c9dca0afe1772f100bcf1f4daf8d444cb92
Torsten Bögershausen <torsten.bogershausen@esss.se>
2021-07-02 05:30:33 +0200

    test/pytests36/980: More tests
    Add more tests, since there are more "network problems" in the
    simulator.
    
    Add one last test:
    Send and invalid network problem to the simulator and check
    that it is "rejected".
    The simulator returns "Error", and the "-DbgStrToMCU" PV goes
    into the "INVALID WRITE" alarm state.
    
    If the simulator ever gets more simulated network problems,
    this last test case will fail and TC 890 needs to be updated.

--- test/pytests36/980_Simulator-networkproblems.py
+++ test/pytests36/980_Simulator-networkproblems.py
@@ -13,7 +13,6 @@
 # What the different failures mean, is defined in the simulator
 
 
-
 import datetime
 import unittest
 import os
@@ -31,15 +30,46 @@ filnam = "9800xx.py"
 maxDelta = 0.5
 timeout = 3.0
 
+
 def lineno():
     return inspect.currentframe().f_back.f_lineno
+
 
 def simulateNetworkProblem(self, tc_no, simulatedNetworkProblem):
     myHighHardLimitPos = tc_no
     self.axisCom.put("-DbgStrToLOG", "Start " + str(tc_no), wait=True)
     self.axisMr.setValueOnSimulator(tc_no, "fHighHardLimitPos", myHighHardLimitPos)
-    self.axisMr.setValueOnSimulator(tc_no, "ads.simulatedNetworkProblem", simulatedNetworkProblem)
-    passed = self.axisMr.waitForValueChanged(tc_no, "-CfgPMAX-RB", myHighHardLimitPos, maxDelta, timeout)
+    self.axisMr.setValueOnSimulator(
+        tc_no, "ads.simulatedNetworkProblem", simulatedNetworkProblem
+    )
+    passed = self.axisMr.waitForValueChanged(
+        tc_no, "-CfgPMAX-RB", myHighHardLimitPos, maxDelta, timeout
+    )
+    if passed:
+        self.axisCom.put("-DbgStrToLOG", "Passed " + str(tc_no), wait=True)
+    else:
+        self.axisCom.put("-DbgStrToLOG", "Failed " + str(tc_no), wait=True)
+    assert passed
+
+
+def invalidNetworkProblem(self, tc_no, simulatedNetworkProblem):
+    myHighHardLimitPos = tc_no
+    self.axisCom.put("-DbgStrToLOG", "Start " + str(tc_no), wait=True)
+    self.axisMr.setValueOnSimulator(tc_no, "fHighHardLimitPos", myHighHardLimitPos)
+    passed = False
+    try:
+        self.axisMr.setValueOnSimulator(
+            tc_no, "ads.simulatedNetworkProblem", simulatedNetworkProblem
+        )
+    except Exception as ex:
+        e_str = str(ex)
+        print(
+            f"{datetime.datetime.now():%Y-%m-%d %H:%M:%S} {tc_no}: invalidNetworkProblem e_str={e_str}"
+        )
+        # STAT == WRITE SEVR == INVALID
+        if e_str == "stat=2 sevr=3":
+            passed = True
+
     if passed:
         self.axisCom.put("-DbgStrToLOG", "Passed " + str(tc_no), wait=True)
     else:
@@ -54,7 +84,7 @@ class Test(unittest.TestCase):
     )
 
     axisCom = AxisCom(url_string, log_debug=False)
-    axisMr = AxisMr(axisCom)
+    axisMr = AxisMr(axisCom, url_string=url_string)
 
     # Reset simulator
     def test_TC_98000(self):
@@ -64,45 +94,59 @@ class Test(unittest.TestCase):
     #
     def test_TC_98001(self):
         tc_no = 98001
-        simulateNetworkProblem(self, tc_no, tc_no - 98000);
+        simulateNetworkProblem(self, tc_no, tc_no - 98000)
 
     def test_TC_98002(self):
         tc_no = 98002
-        simulateNetworkProblem(self, tc_no, tc_no - 98000);
+        simulateNetworkProblem(self, tc_no, tc_no - 98000)
 
     def test_TC_98003(self):
         tc_no = 98003
-        simulateNetworkProblem(self, tc_no, tc_no - 98000);
+        simulateNetworkProblem(self, tc_no, tc_no - 98000)
 
     def test_TC_98004(self):
         tc_no = 98004
-        simulateNetworkProblem(self, tc_no, tc_no - 98000);
+        simulateNetworkProblem(self, tc_no, tc_no - 98000)
 
     def test_TC_98005(self):
         tc_no = 98005
-        simulateNetworkProblem(self, tc_no, tc_no - 98000);
+        simulateNetworkProblem(self, tc_no, tc_no - 98000)
 
     def test_TC_98006(self):
         tc_no = 98006
-        simulateNetworkProblem(self, tc_no, tc_no - 98000);
+        simulateNetworkProblem(self, tc_no, tc_no - 98000)
 
     def test_TC_98007(self):
         tc_no = 98007
-        simulateNetworkProblem(self, tc_no, tc_no - 98000);
+        simulateNetworkProblem(self, tc_no, tc_no - 98000)
 
     def test_TC_98008(self):
         tc_no = 98008
-        simulateNetworkProblem(self, tc_no, tc_no - 98000);
+        simulateNetworkProblem(self, tc_no, tc_no - 98000)
 
     def test_TC_98009(self):
         tc_no = 98009
-        simulateNetworkProblem(self, tc_no, tc_no - 98000);
+        simulateNetworkProblem(self, tc_no, tc_no - 98000)
 
-#    def test_TC_98010(self):
-#        tc_no = 98010
-#        simulateNetworkProblem(self, tc_no, tc_no - 98000);
+    def test_TC_98010(self):
+        tc_no = 98010
+        simulateNetworkProblem(self, tc_no, tc_no - 98000)
 
-#    def test_TC_98011(self):
-#        tc_no = 98011
-#        simulateNetworkProblem(self, tc_no, tc_no - 98000);
+    def test_TC_98011(self):
+        tc_no = 98011
+        simulateNetworkProblem(self, tc_no, tc_no - 98000)
 
+    def test_TC_98012(self):
+        tc_no = 98012
+        simulateNetworkProblem(self, tc_no, tc_no - 98000)
+
+    def test_TC_98013(self):
+        tc_no = 98013
+        simulateNetworkProblem(self, tc_no, tc_no - 98000)
+
+    # The last test case uses an invalid simulatedNetworkProblem
+    # The simulator will not return "OK", and the PV will go into
+    # an INVALID WRITE SEVR/STAT alarm
+    def test_TC_98099(self):
+        tc_no = 98099
+        invalidNetworkProblem(self, tc_no, tc_no - 98000)
