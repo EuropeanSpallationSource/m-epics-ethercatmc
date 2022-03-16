be5400fbdf4a5a6a74738de7794cde4d16a497d3
Torsten Bögershausen <torsten.bogershausen@esss.se>
2022-03-16 13:41:26 +0100

    test: Rename test case 220
    Test case 220 was wronlgy named:
    We need the additional records to controll the auto-power-on feature.
    Rename it.
    Add one test case to test a race condition found in the motor module,
    when the "power off delay" was set to 0.0
    
    Changes to be committed:
      renamed:    220_Record_auto_power.py -> 220_Ethercat-auto-power.py
      modified:   Makefile

--- test/pytests36/220_Ethercat-auto-power.py
+++ test/pytests36/220_Ethercat-auto-power.py
@@ -14,7 +14,9 @@ filnam = "220xx.py"
 ###
 
 PwrOnDly = 6.0
-PwrOffDly = 3.0
+globalPwrOffDly = 3.0
+DLY = 4.0
+BDST = 5.0
 
 
 def restorePwrSettings(self, tc_no, dly, bdst, pwrAuto, pwrOnDly, pwrOffDly):
@@ -25,25 +27,32 @@ def restorePwrSettings(self, tc_no, dly, bdst, pwrAuto, pwrOnDly, pwrOffDly):
     self.axisCom.put("-PwrOffDly", pwrOffDly)
 
 
-def do_220_autopower(self, tc_no, autopower):
+def do_220_autopower(self, tc_no, autopower, pwrOffDly):
     self.axisCom.putDbgStrToLOG("Start " + tc_no[0:20], wait=True)
     self.axisMr.setCNENandWait(tc_no, 0)
     self.axisCom.put("-PwrAuto", autopower)
     self.axisCom.put("-PwrOnDly", PwrOnDly)
-    self.axisCom.put("-PwrOffDly", PwrOffDly)
+    self.axisCom.put("-PwrOffDly", pwrOffDly)
+    bdst = self.axisCom.get(".BDST")
+    dly = self.axisCom.get(".DLY")
+    destination = self.saved_LLM + bdst + 2 * (int(tc_no) - 2200) + 2 * autopower
     print(
-        f"{datetime.datetime.now():%Y-%m-%d %H:%M:%S} {filnam} {tc_no} Enable move to LLM +10"
+        f"{datetime.datetime.now():%Y-%m-%d %H:%M:%S} {filnam} {tc_no} destination={destination}"
     )
-    destination = self.saved_LLM + self.saved_BDST + 1 + 10 + 2 * autopower
     self.axisMr.moveWait(tc_no, destination)
 
-    # Make sure drive is still enabled
-    power1 = self.axisCom.get(".CNEN", use_monitor=False)
-    print(
-        f"{datetime.datetime.now():%Y-%m-%d %H:%M:%S} {filnam} {tc_no} Check drive is still enabled power1={int(power1)}"
+    if dly < pwrOffDly:
+        # Make sure drive is still enabled
+        power1 = self.axisCom.get(".CNEN", use_monitor=False)
+        print(
+            f"{datetime.datetime.now():%Y-%m-%d %H:%M:%S} {filnam} {tc_no} Check drive is still enabled power1={int(power1)}"
+        )
+    else:
+        power1 = 1
+    time.sleep(globalPwrOffDly + pwrOffDly + 2.0)
+    self.axisMr.waitForPowerOff(
+        tc_no + "autopower", 2 * (globalPwrOffDly + float(pwrOffDly) + 2.0)
     )
-
-    time.sleep(PwrOnDly + PwrOffDly + 2.0)
     power2 = self.axisCom.get(".CNEN", use_monitor=False)
     print(
         f"{datetime.datetime.now():%Y-%m-%d %H:%M:%S} {filnam} {tc_no} Wait 8s and check drive is now disabled power2={int(power2)}"
@@ -59,14 +68,19 @@ def do_220_autopower(self, tc_no, autopower):
     )
     self.axisMr.setCNENandWait(tc_no, self.saved_CNEN)
 
+    msta = int(self.axisCom.get(".MSTA"))
     print(
         f"{datetime.datetime.now():%Y-%m-%d %H:%M:%S} {filnam} {tc_no} power1={int(power1)} power2={int(power2)}"
+    )
+    print(
+        f"{datetime.datetime.now():%Y-%m-%d %H:%M:%S} {filnam} {tc_no}  msta={self.axisMr.getMSTAtext(msta)}"
     )
     if (power1 == 1) and (power2 == 0):
         testPassed = True
     else:
         testPassed = False
-
+    if (msta & self.axisMr.MSTA_BIT_PROBLEM) != 0:
+        testPassed = False
     if testPassed:
         self.axisCom.putDbgStrToLOG("Passed " + str(tc_no), wait=True)
     else:
@@ -108,7 +122,7 @@ class Test(unittest.TestCase):
         self.axisCom.put("-PwrAuto", 2)
         self.axisCom.put("-PwrOnDly", PwrOnDly)
         self.axisMr.setCNENandWait(tc_no, 1)
-        destination = self.saved_LLM + self.saved_BDST + 1
+        destination = self.saved_LLM + 1
         self.axisMr.moveWait(tc_no, destination)
         restorePwrSettings(
             self,
@@ -126,15 +140,29 @@ class Test(unittest.TestCase):
         print(
             f"{datetime.datetime.now():%Y-%m-%d %H:%M:%S} {filnam} {tc_no} autopower "
         )
-        do_220_autopower(self, tc_no, 1)
+        do_220_autopower(self, tc_no, 1, globalPwrOffDly)
 
     def test_TC_2203(self):
         tc_no = "2203"
         print(
             f"{datetime.datetime.now():%Y-%m-%d %H:%M:%S} {filnam} {tc_no} autopower "
         )
-        do_220_autopower(self, tc_no, 2)
+        do_220_autopower(self, tc_no, 2, globalPwrOffDly)
 
     def test_TC_2204(self):
         tc_no = "2204"
+        print(
+            f"{datetime.datetime.now():%Y-%m-%d %H:%M:%S} {filnam} {tc_no} autopower "
+        )
+        self.axisCom.put(".DLY", DLY)
+        self.axisCom.put(".BDST", BDST)
+        do_220_autopower(self, tc_no, 2, 0.0)
+
+    def test_TC_2205(self):
+        tc_no = "2205"
+        msta = int(self.axisCom.get(".MSTA", use_monitor=False))
+        print(f"{tc_no} Error msta={self.axisMr.getMSTAtext(msta)}")
+        if msta & self.axisMr.MSTA_BIT_PROBLEM:
+            self.axisMr.resetAxis(tc_no)
+
         self.axisMr.setCNENandWait(tc_no, self.saved_CNEN)
