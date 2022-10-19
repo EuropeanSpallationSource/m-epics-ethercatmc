a9e9ca502327fb95d363e00e2f8594adb52ab3ef
Torsten Bögershausen <torsten.bogershausen@esss.se>
2022-10-19 14:56:54 +0200

    More debugging with EL6688 and EL1252
    Add a new PV: TimeOffsetDiffEL6688
    Improve ethercatmcEL1252-OneChannel.iocsh and ethercatmcEL6688-PTP.iocsh:
    Create the needed asynParameters in case we connect "late to the crate"
    (After loading the records. We always do this)
    
    Changes to be committed:
        modified:   ethercatmcApp/Db/ethercatmcEL6688-ExtSync.template
        modified:   iocsh/ethercatmcEL1252-OneChannel.iocsh
        modified:   iocsh/ethercatmcEL6688-ExtSync.iocsh
        modified:   iocsh/ethercatmcEL6688-PTP.iocsh
        new file:   test/pythonscripts/monEL6688-1252-crate007.sh

---
+++ test/pythonscripts/monEL6688-1252-crate007.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+P="LabS-MCAG:MC-MCU-07:"
+if ! type pvmonitor >/dev/null 2>&1; then
+  . ~/MCAG_SetupMotionDemo/.epics.ymir-vm-ioc-01.cn.nin.ess.eu.Linux.x86_64
+fi
+
+if ! type python3 >/dev/null 2>&1; then
+  echo >&2 python3 not found. Please run
+  echo >&2 conda activate pyepicsPytestPVApy
+  exit 1
+fi
+
+# The log file name is dependent on the name of this script
+mewithoutdir="${0##*/}"
+basename="${mewithoutdir%.*}"
+LOGFILE=${basename}.txt
+echo "##MYBASENAME=$MYBASENAME"
+echo "##LOGFILE=$LOGFILE"
+
+if test -f $LOGFILE; then
+  timestamp=$(date "+%y-%m-%d-%H.%M.%S")
+  mkdir -p logs
+  mv $LOGFILE ./logs/$timestamp-$LOGFILE || exit 1
+fi
+
+pvmonitor ${P}PTPState ${P}PTPOffset \
+${P}DcToExtTimeOffsetEL6688 \
+${P}DcToExtTimeOffsetSystem  \
+${P}WriteDoneExtTimeOffset  \
+${P}TimeOffsetDiffEL6688 \
+${P}UTCEL1252P0 | ./tai2string.py  2>&1 | tee $LOGFILE
