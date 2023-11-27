9bc6f8ea34351e205b0025bb39e36e087672385e
Torsten Bögershausen <torsten.bogershausen@esss.se>
2023-11-27 17:23:36 +0100

    checkshellcheck.sh: use git ls-files, if possible
    When running in a worktree with git, use `git ls-files` to find
    shell files.
    This will exclude files from any venv/ directory or other local stuff.
    
    When running in a ci environment, use the old `find`, as git may
    not be available

--- checkshellcheck.sh
+++ checkshellcheck.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
-find test -name "*.sh" |
-  grep -v checkws.sh |
-  xargs shellcheck --format=gcc -x
+if type git >/dev/null 2>&1; then
+  git ls-files "*.sh" |
+    grep -v "ethercatmcExApp/op" |
+    xargs shellcheck --format=gcc -x
+else
+  find . -name "*.sh" |
+    grep -v -E "ethercatmcExApp/op|.git" |
+    xargs shellcheck --format=gcc -x
+fi
