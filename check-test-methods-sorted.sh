7894f6159f6810c58ddad39e2eda58dccc72e8fd
Torsten Bögershausen <torsten.bogershausen@esss.se>
2024-02-16 08:29:43 +0100

    gitlab-ci: Fix breakage; activate check-test-methods-sorted
    Fix a breakage around shfmt, found by ci.
    While there, activate test/pytests36/check-methods-sorted.sh
    in a new script check-test-methods-sorted.sh
    
    And, while there, remove the case-insensitve comparison in
    check-methods-sorted.sh

---
+++ check-test-methods-sorted.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+cd test/pytests36 && ./check-methods-sorted.sh
--- test/pytests36/check-methods-sorted.sh
+++ test/pytests36/check-methods-sorted.sh
@@ -10,21 +10,21 @@ export LC_ALL
 files=$(git ls-files 'A*.py')
 if test -n "$files"; then
   for file in $files; do
-    if ! test -s $file; then
+    if ! test -s "$file"; then
       continue
     fi
     rm -f /tmp/$$sorted /tmp/$$unsorted
-    if grep -q "class Test:" $file; then
+    if grep -q "class Test:" "$file"; then
       continue
     fi
-    grep "^    def" $file |
+    grep "^    def" "$file" |
       sed -e "s/__\([a-zA-Z]*\)__/@@\1@@/g" >/tmp/$$unsorted
     sed -e "s/__\([a-zA-Z]*\)__/@@\1@@/g" </tmp/$$unsorted |
-      sort -f  >/tmp/$$sorted
+      sort >/tmp/$$sorted
     diff /tmp/$$sorted /tmp/$$unsorted
     res=$?
     if test $res != 0; then
-      echo >&2 $file is not sorted
+      echo >&2 "$file" is not sorted
       cat /tmp/$$sorted | sed -e "s/@@\([a-zA-Z]*\)@@/__\1__/g"
       rm /tmp/$$sorted /tmp/$$unsorted
       exit 1
