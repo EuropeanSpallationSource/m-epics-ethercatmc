fe9367d6660ebd7d6c474664d755317fa3784906
Torsten Bögershausen <torsten.bogershausen@esss.se>
2024-08-13 12:52:51 +0200

    op/Boy: Prepare for custom pils parameters
    Add a helper script to add additional "config" parameters:
    genAxisConfigCutomPara.sh
    
    Add it into the Makefile, to re-generate the same
    ethercatmcaxisConfig-pils.opi
    as before.
    More changes in the next commit(s)
    Changes to be committed:
        modified:   Makefile
        new file:   ethercatmcaxisConfigCustomPara.mid
        new file:   genAxisConfigCutomPara.sh

---
+++ ethercatmcExApp/op/Boy/tools/genAxisConfigCutomPara.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+# Name of the destination file on disk
+FILE=$1
+shift
+
+cat ethercatmcaxisConfig.start ethercatmcaxisConfig-pils.mid >/tmp/$$ &&
+y=420
+# pick up all arguments
+PARAM="$1"
+while test "$PARAM" != ""; do
+  cmd=$(echo ./shiftopi.py --shifty $y)
+    echo $0: cmd=$cmd
+    eval $cmd <ethercatmcaxisConfigCustomPara.mid |
+      sed -e "s!CfgXXYYZZ!$PARAM!" >>/tmp/$$
+    shift
+    PARAM="$1"
+    y=$(($y + 36))
+done
+
+
+touch "$FILE" &&
+  chmod +w "$FILE" &&
+  cat ethercatmcaxisConfig.end  >>/tmp/$$ &&
+  sed -e "s!ethercatmcaxisConfig-pils.opi!$FILE!" </tmp/$$ >"$FILE" &&
+  chmod -w "$FILE" &&
+  rm /tmp/$$
+
