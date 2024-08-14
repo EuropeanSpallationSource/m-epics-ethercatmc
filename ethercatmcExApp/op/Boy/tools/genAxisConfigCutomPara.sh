faebf8c26a440b54add0ee833510b42b77bcfc9e
Torsten Bögershausen <torsten.bogershausen@esss.se>
2024-08-14 09:16:58 +0200

    Run shfmt on genAxisConfigCutomPara.sh Changes to be committed:     modified:   ethercatmcExApp/op/Boy/tools/genAxisConfigCutomPara.sh

--- ethercatmcExApp/op/Boy/tools/genAxisConfigCutomPara.sh
+++ ethercatmcExApp/op/Boy/tools/genAxisConfigCutomPara.sh
@@ -5,24 +5,22 @@ FILE=$1
 shift
 
 cat ethercatmcaxisConfig.start ethercatmcaxisConfig-pils.mid >/tmp/$$ &&
-y=420
+  y=420
 # pick up all arguments
 PARAM="$1"
 while test "$PARAM" != ""; do
   cmd=$(echo ./shiftopi.py --shifty $y)
-    echo $0: cmd=$cmd
-    eval $cmd <ethercatmcaxisConfigCustomPara.mid |
-      sed -e "s!CfgXXYYZZ!$PARAM!" >>/tmp/$$
-    shift
-    PARAM="$1"
-    y=$(($y + 20))
+  echo $0: cmd=$cmd
+  eval $cmd <ethercatmcaxisConfigCustomPara.mid |
+    sed -e "s!CfgXXYYZZ!$PARAM!" >>/tmp/$$
+  shift
+  PARAM="$1"
+  y=$(($y + 20))
 done
-
 
 touch "$FILE" &&
   chmod +w "$FILE" &&
-  cat ethercatmcaxisConfig.end  >>/tmp/$$ &&
+  cat ethercatmcaxisConfig.end >>/tmp/$$ &&
   sed -e "s!ethercatmcaxisConfig-pils.opi!$FILE!" </tmp/$$ >"$FILE" &&
   chmod -w "$FILE" &&
   rm /tmp/$$
-
