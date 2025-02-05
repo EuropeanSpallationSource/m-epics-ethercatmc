1a0de2707f6f224ff3af3538a7a422a4749d6094
Torsten Bögershausen <torsten.bogershausen@esss.se>
2025-02-05 06:26:17 +0100

    test/.../msta2string.py: Add support for MIP
    Add a decoder for the .MIP field in the motorRecord, when
    a log file from camonitor is used.
    Rename handle_camonitor_line() into handle_camonitor_msta_line()
    and add its sibbling handle_camonitor_mip_line().
    
    While there, add the missing direction bit for MSTA

--- test/pythonscripts/msta2string.py
+++ test/pythonscripts/msta2string.py
@@ -22,6 +22,61 @@ MSTA_BIT_DONE = 1 << (2 - 1)  # 0002
 MSTA_BIT_DIRECTION = 1 << (1 - 1)  # 0001
 
 
+# MIP_DONE        0x0000  /* No motion is in progress. */
+MIP_JOGF = 1 << (1 - 1)  # 0001
+MIP_JOGR = 1 << (2 - 1)  # 0001
+MIP_JOG_BL1 = 1 << (3 - 2)  # 0002
+MIP_HOMF = 1 << (4 - 1)  # 0004
+MIP_HOMR = 1 << (5 - 1)  # 0008
+MIP_MOVE = 1 << (6 - 1)  # 0010
+MIP_RETRY = 1 << (7 - 1)  # 0020
+MIP_LOAD_P = 1 << (8 - 1)  # 0040
+MIP_MOVE_BL = 1 << (9 - 1)  # 0080
+MIP_STOP = 1 << (10 - 1)  # 0100
+MIP_DELAY_ACK = 1 << (11 - 1)  # 0200
+MIP_JOG_REQ = 1 << (12 - 1)  # 0400
+MIP_JOG_STOP = 1 << (13 - 1)  # 0800
+MIP_JOG_BL2 = 1 << (14 - 1)  # 1000
+MIP_EXTERNAL = 1 << (15 - 1)  # 2000
+
+
+def get_mip_text_actual(mip):
+    if mip == 0:
+        return "Done"
+    ret = ""
+    if mip & MIP_JOGF:
+        ret = ret + "Jogf"
+    if mip & MIP_JOGR:
+        ret = ret + "Jogr"
+    if mip & MIP_JOG_BL1:
+        ret = ret + "JogBL1"
+    if mip & MIP_HOMF:
+        ret = ret + "Homf"
+    if mip & MIP_HOMR:
+        ret = ret + "Homr"
+    if mip & MIP_MOVE:
+        ret = ret + "Move"
+    if mip & MIP_RETRY:
+        ret = ret + "Retry"
+    if mip & MIP_LOAD_P:
+        ret = ret + "Loadp"
+    if mip & MIP_MOVE_BL:
+        ret = ret + "MovBl"
+    if mip & MIP_STOP:
+        ret = ret + "Stop"
+    if mip & MIP_DELAY_ACK:
+        ret = ret + "Delayack"
+    if mip & MIP_JOG_REQ:
+        ret = ret + "Jogreq"
+    if mip & MIP_JOG_STOP:
+        ret = ret + "Stop"
+    if mip & MIP_JOG_BL2:
+        ret = ret + "BL2"
+    if mip & MIP_EXTERNAL:
+        ret = ret + "External"
+    return ret
+
+
 def get_msta_text_actual(msta):
     ret = ""
     if msta & MSTA_BIT_HOMED:
@@ -146,6 +201,11 @@ def get_msta_text_delta(old_msta, new_msta):
             ret = ret + "+Done"
         else:
             ret = ret + "-Done"
+    if (old_msta ^ new_msta) & MSTA_BIT_DIRECTION:
+        if new_msta & MSTA_BIT_DIRECTION:
+            ret = ret + "+Direction"
+        else:
+            ret = ret + "-Direction"
 
     return ret
 
@@ -166,6 +226,12 @@ RE_MATCH_MSTA_CAMONITOR_DATE = re.compile(
 RE_MATCH_MSTA_CAMONITOR_UNDEFINED = re.compile(
     "(\S+\.MSTA)\s+([^0-9:. ]+)\s+([0-9]+)\s*(.*)$"
 )
+
+# SQ:SINQTEST:mmacs1:lin1.MIP    2025-02-04 18:18:19.694284 0
+RE_MATCH_MIP_CAMONITOR_DATE = re.compile(
+    "(\S+\.MIP)\s+([-0-9]+\s+[0-9.:]+)\s+([0-9]+)\s*(.*)$"
+)
+
 
 last_msta_per_pv = {}  # Save the (last) value in order to calc a diff
 
@@ -217,7 +283,7 @@ def handle_ioclog_msta_line(match_msta_ioclog_msta):
     sys.stdout.flush()
 
 
-def handle_camonitor_line(match_msta_camonitor):
+def handle_camonitor_msta_line(match_msta_camonitor):
     gidx = 1
     pvname = match_msta_camonitor.group(gidx)
     gidx = gidx + 1
@@ -235,6 +301,20 @@ def handle_camonitor_line(match_msta_camonitor):
         msta_delta = get_msta_text_delta(last_msta_int, msta_val_int)
         print(f"O:{pvname:30s} {time_date} 0x{msta_val_int:04x} ({msta_delta}){rest}")
     last_msta_per_pv[pvname] = msta_val_int
+    sys.stdout.flush()
+
+
+def handle_camonitor_mip_line(match_mip_camonitor):
+    gidx = 1
+    pvname = match_mip_camonitor.group(gidx)
+    gidx = gidx + 1
+    time_date = match_mip_camonitor.group(gidx)
+    gidx = gidx + 1
+    mip_val_int = int(match_mip_camonitor.group(gidx))
+    gidx = gidx + 1
+    rest = match_mip_camonitor.group(gidx)
+    mip_str = get_mip_text_actual(mip_val_int)
+    print(f"M:{pvname:30s} {time_date} 0x{mip_val_int:04x} ({mip_str}){rest}")
     sys.stdout.flush()
 
 
@@ -258,7 +338,11 @@ def main(argv=None):
         if match_msta_camonitor is None:
             match_msta_camonitor = RE_MATCH_MSTA_CAMONITOR_UNDEFINED.match(line)
         if match_msta_camonitor is not None:
-            handle_camonitor_line(match_msta_camonitor)
+            handle_camonitor_msta_line(match_msta_camonitor)
+            continue
+        match_mip_camonitor = RE_MATCH_MIP_CAMONITOR_DATE.match(line)
+        if match_mip_camonitor is not None:
+            handle_camonitor_mip_line(match_mip_camonitor)
             continue
         print(f"L:{line}")
         sys.stdout.flush()
