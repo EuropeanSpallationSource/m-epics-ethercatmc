82a2f21aa01079f4c57a3d648e2ce84f79f37572
Torsten Bögershausen <torsten.bogershausen@esss.se>
2025-08-15 15:04:54 +0200

    t/941: Rewrite to use statusReasonAux
    Summarize the bits "error", "(not) homed", "power on"
    into a statusReasonAux word.
    
    This prepares the next test cases/commits.
    No changes otherwise.

--- test/pytests36/941_Simulator-MsgTxt-ErrTxt.py
+++ test/pytests36/941_Simulator-MsgTxt-ErrTxt.py
@@ -14,27 +14,28 @@ filnam = os.path.basename(__file__)[0:3]
 ###
 
 polltime = 0.05
-idxStatusCodeRESET = 0x00000000
-idxStatusCodeIDLE = 0x10000000
-idxStatusCodePOWEROFF = 0x20000000
-idxStatusCodeWARN = 0x30000000
-idxStatusCodeERR4 = 0x40000000
-idxStatusCodeSTART = 0x50000000
-idxStatusCodeBUSY = 0x60000000
-idxStatusCodeSTOP = 0x70000000
-idxStatusCodeERROR = 0x80000000
-idxStatusCodeERR9 = 0x90000000
-idxStatusCodeERR10 = 0xA0000000
-idxStatusCodeERR11 = 0xB0000000
-idxStatusCodeERR12 = 0xC0000000
-idxStatusCodeERR13 = 0xD0000000
-idxStatusCodeERR14 = 0xE0000000
-idxStatusCodeERR15 = 0xF0000000
+idxStatusCodeRESET = "0x00000000"
+idxStatusCodeIDLE = "0x10000000"
+statusReasonAuxIdlePowerOn = "0x10400000"
+idxStatusCodePOWEROFF = "0x20000000"
+idxStatusCodeWARN = "0x30000000"
+idxStatusCodeERR4 = "0x40000000"
+idxStatusCodeSTART = "0x50000000"
+idxStatusCodeBUSY = "0x60000000"
+idxStatusCodeSTOP = "0x70000000"
+idxStatusCodeERROR = "0x80000000"
+idxStatusCodeERR9 = "0x90000000"
+idxStatusCodeERR10 = "0xA0000000"
+idxStatusCodeERR11 = "0xB0000000"
+idxStatusCodeERR12 = "0xC0000000"
+idxStatusCodeERR13 = "0xD0000000"
+idxStatusCodeERR14 = "0xE0000000"
+idxStatusCodeERR15 = "0xF0000000"
 
-idxReasonBitHigh = 0x08000000
-idxReasonBitLow = 0x04000000
-idxReasonBitTimeout = 0x02000000
-idxReasonBitInhibit = 0x01000000
+idxReasonBitHigh = "0x08000000"
+idxReasonBitLow = "0x04000000"
+idxReasonBitTimeout = "0x02000000"
+idxReasonBitInhibit = "0x01000000"
 
 
 # EPICS severities
@@ -119,29 +120,41 @@ alarmStatSevrValuesInt = {
 }
 
 
-#   Test case number, error, homed, autoPower, amplifier on, ErrTxt
-#   tc_no     e  errId  hm au on  MsgTxt                ErrTxt
+#   Test case number, error, autoPower, ErrTxt
+#   tc_no     e  errId  au  MsgTxt                ErrTxt
 allTCs = [
-    (9410001, 0, 0x0000, 0, 0, 0, "E: PowerOff", "E: PowerOff"),
-    (9410002, 0, 0x0000, 0, 0, 1, "E: Axis not homed", "E: Axis not homed"),
-    (9410003, 0, 0x0000, 0, 1, 0, "E: Axis not homed", "E: Axis not homed"),
-    (9410004, 0, 0x0000, 0, 1, 1, "E: Axis not homed", "E: Axis not homed"),
-    (9410005, 0, 0x4460, 0, 0, 0, "E: PowerOff", "E: PowerOff"),
-    (9410006, 0, 0x4460, 0, 0, 1, "E: Axis not homed", "E: Axis not homed"),
-    (9410007, 0, 0x4460, 0, 1, 0, "E: Axis not homed", "E: Axis not homed"),
-    (9410008, 0, 0x4460, 0, 1, 1, "E: Axis not homed", "E: Axis not homed"),
-    (9410009, 0, 0x4460, 1, 0, 0, "E: PowerOff", "E: PowerOff"),
-    (9410010, 0, 0x4460, 1, 0, 1, "W: Low soft lim 4460", ""),
-    (9410011, 0, 0x4460, 1, 1, 0, "W: Low soft lim 4460", ""),
-    (9410012, 0, 0x4460, 1, 1, 1, "W: Low soft lim 4460", ""),
-    (9410020, 1, 0x4550, 0, 0, 0, "E: PowerOff", "E: PowerOff"),
-    (9410021, 1, 0x4550, 0, 0, 1, "E: Follw errpos 4550", "E: Follw errpos 4550"),
-    (9410022, 1, 0x4550, 0, 1, 0, "E: Follw errpos 4550", "E: Follw errpos 4550"),
-    (9410023, 1, 0x4550, 0, 1, 1, "E: Follw errpos 4550", "E: Follw errpos 4550"),
-    (9410024, 1, 0x4550, 1, 0, 0, "E: PowerOff", "E: PowerOff"),
-    (9410025, 1, 0x4550, 1, 0, 1, "E: Follw errpos 4550", "E: Follw errpos 4550"),
-    (9410026, 1, 0x4550, 1, 1, 0, "E: Follw errpos 4550", "E: Follw errpos 4550"),
-    (9410027, 1, 0x4550, 1, 1, 1, "E: Follw errpos 4550", "E: Follw errpos 4550"),
+    # 1..7: Not homed
+    # 1: not homed, not enabled, autopower off
+    (9410001, "0x10800000", 0x0000, 0, "E: PowerOff", "E: PowerOff"),
+    # 2: not homed, enabled, autopower off
+    (9410002, "0x10C00000", 0x0000, 0, "E: Axis not homed", "E: Axis not homed"),
+    # 3: not homed, not enabled, autopower on
+    (9410003, "0x10800000", 0x0000, 1, "E: Axis not homed", "E: Axis not homed"),
+    # 4: not homed, enabled, autopower on
+    (9410004, "0x10C00000", 0x0000, 1, "E: Axis not homed", "E: Axis not homed"),
+    # 5: not homed, not enabled, autopower off, error Id
+    (9410005, "0x10800000", 0x4460, 0, "E: PowerOff", "E: PowerOff"),
+    # 6: not homed, enabled, autopower off, error Id
+    (9410006, "0x10C00000", 0x4460, 0, "E: Axis not homed", "E: Axis not homed"),
+    # 7: not homed, not enabled, autopower on, error Id
+    (9410007, "0x10800000", 0x4460, 1, "E: Axis not homed", "E: Axis not homed"),
+    # 8: not homed, enabled, autopower on, error Id
+    (9410008, "0x10C00000", 0x4460, 1, "E: Axis not homed", "E: Axis not homed"),
+    # 9..10 homed, not enabled/enabled, autopower off, error Id
+    (9410009, "0x10000000", 0x4460, 0, "E: PowerOff", "E: PowerOff"),
+    (9410010, "0x10400000", 0x4460, 0, "W: Low soft lim 4460", ""),
+    # 11..12 homed, not enabled/enabled, autopower on, error Id
+    (9410011, "0x10000000", 0x4460, 1, "W: Low soft lim 4460", ""),
+    (9410012, "0x10400000", 0x4460, 1, "W: Low soft lim 4460", ""),
+    # Error state
+    (9410020, "0x80000000", 0x4550, 0, "E: PowerOff", "E: PowerOff"),
+    (9410021, "0x80400000", 0x4550, 0, "E: Follw errpos 4550", "E: Follw errpos 4550"),
+    (9410022, "0x80000000", 0x4550, 1, "E: Follw errpos 4550", "E: Follw errpos 4550"),
+    (9410023, "0x80400000", 0x4550, 1, "E: Follw errpos 4550", "E: Follw errpos 4550"),
+    (9410024, "0x80000000", 0x4550, 0, "E: PowerOff", "E: PowerOff"),
+    (9410025, "0x80400000", 0x4550, 0, "E: Follw errpos 4550", "E: Follw errpos 4550"),
+    (9410026, "0x80000000", 0x4550, 1, "E: Follw errpos 4550", "E: Follw errpos 4550"),
+    (9410027, "0x80400000", 0x4550, 1, "E: Follw errpos 4550", "E: Follw errpos 4550"),
 ]
 
 
@@ -152,37 +165,28 @@ def lineno():
 def writeBitsReadMsgTxtErrTxt(
     self,
     tc_no=0,
-    axisHomed=-1,
-    err=-1,
+    statusReasonAux=None,
     errorId=-1,
     pwrAuto=-1,
-    amplifierOn=-1,
     expMsgTxt="undef",
     expErrTxt="undef",
 ):
     self.axisCom.putDbgStrToLOG("Start " + str(tc_no), wait=True)
     assert tc_no != 0
-    assert axisHomed >= 0
-    assert err != -1
+    assert statusReasonAux is not None
     assert errorId != -1
     assert pwrAuto != -1
-    assert amplifierOn != -1
     assert expMsgTxt != "undef"
     assert expErrTxt != "undef"
 
-    if amplifierOn == 1:
-        amplifierPercent = 100
-    else:
-        amplifierPercent = 0
     maxTime = 5  # 5 seconds maximum to let read only parameters ripple through
     passed = False
+    self.axisMr.setValueOnSimulator(tc_no, "nStatReasAUX", statusReasonAux)
+    self.axisMr.setValueOnSimulator(tc_no, "bManualSimulatorMode", 1)
+
+    self.axisMr.setValueOnSimulator(tc_no, "nErrorId", errorId)
     oldPwrAuto = self.axisCom.get("-PwrAuto")
     self.axisCom.put("-PwrAuto", pwrAuto)
-    self.axisMr.setValueOnSimulator(tc_no, "bManualSimulatorMode", 1)
-    self.axisMr.setValueOnSimulator(tc_no, "bAxisHomed", axisHomed)
-    self.axisMr.setValueOnSimulator(tc_no, "bError", err)
-    self.axisMr.setValueOnSimulator(tc_no, "nErrorId", errorId)
-    self.axisMr.setValueOnSimulator(tc_no, "nAmplifierPercent", amplifierPercent)
     self.axisCom.put(".STUP", 1)
     while maxTime > 0:
         actMsgTxt = self.axisCom.get("-MsgTxt")
@@ -190,7 +194,7 @@ def writeBitsReadMsgTxtErrTxt(
             actMsgTxt = ""
         actErrTxt = self.axisCom.get("-ErrTxt")
         print(
-            f"{datetime.datetime.now():%Y-%m-%d %H:%M:%S} {filnam}:{lineno()} tc_no={tc_no} maxTime={maxTime:.2f} passed={passed} expMsgTxt='{expMsgTxt}' actMsgTxt={actMsgTxt!r} expErrTxt='{expErrTxt}' actErrTxt={actErrTxt!r}"
+            f"{datetime.datetime.now():%Y-%m-%d %H:%M:%S} {filnam}:{lineno()} tc_no={tc_no} maxTime={maxTime:.2f} expMsgTxt='{expMsgTxt}' actMsgTxt={actMsgTxt!r} expErrTxt='{expErrTxt}' actErrTxt={actErrTxt!r}"
         )
 
         if actMsgTxt == expMsgTxt and actErrTxt == expErrTxt:
@@ -201,7 +205,7 @@ def writeBitsReadMsgTxtErrTxt(
             maxTime = maxTime - polltime
 
     print(
-        f"{datetime.datetime.now():%Y-%m-%d %H:%M:%S} {filnam}:{lineno()} tc_no={tc_no} passed={passed} axisHomed={axisHomed} err={err} errorId={errorId:x} pwrAuto={pwrAuto} amplifierOn={amplifierOn} expMsgTxt='{expMsgTxt}' actMsgTxt={actMsgTxt!r} expErrTxt='{expErrTxt}' actErrTxt={actErrTxt!r}"
+        f"{datetime.datetime.now():%Y-%m-%d %H:%M:%S} {filnam}:{lineno()} tc_no={tc_no} passed={passed} statusReasonAux={statusReasonAux} errorId={errorId:x} pwrAuto={pwrAuto} ampliexpMsgTxt='{expMsgTxt}' actMsgTxt={actMsgTxt!r} expErrTxt='{expErrTxt}' actErrTxt={actErrTxt!r}"
     )
     self.axisCom.put("-PwrAuto", oldPwrAuto)
     self.axisMr.setValueOnSimulator(tc_no, "bManualSimulatorMode", 0)
@@ -227,32 +231,26 @@ class Test(unittest.TestCase):
     def test_TC_94000(self):
         for tc in allTCs:
             tc_no = tc[0]
-            err = tc[1]
+            statusReasonAux = tc[1]
             errorId = tc[2]
-            axisHomed = tc[3]
-            pwrAuto = tc[4]
-            amplifierOn = tc[5]
-            expMsgTxt = tc[6]
-            expErrTxt = tc[7]
+            pwrAuto = tc[3]
+            expMsgTxt = tc[4]
+            expErrTxt = tc[5]
             writeBitsReadMsgTxtErrTxt(
                 self,
                 tc_no=tc_no * 10,
-                axisHomed=1,
-                err=0,
+                statusReasonAux=statusReasonAuxIdlePowerOn,
                 errorId=0,
                 pwrAuto=1,
-                amplifierOn=1,
                 expMsgTxt="",
                 expErrTxt="",
             )
             writeBitsReadMsgTxtErrTxt(
                 self,
                 tc_no=tc_no * 10 + 1,
-                axisHomed=axisHomed,
-                err=err,
+                statusReasonAux=statusReasonAux,
                 errorId=errorId,
                 pwrAuto=pwrAuto,
-                amplifierOn=amplifierOn,
                 expMsgTxt=expMsgTxt,
                 expErrTxt=expErrTxt,
             )
@@ -262,11 +260,9 @@ class Test(unittest.TestCase):
         writeBitsReadMsgTxtErrTxt(
             self,
             tc_no=tc_no,
-            axisHomed=1,
-            err=0,
+            statusReasonAux=statusReasonAuxIdlePowerOn,
             errorId=0,
             pwrAuto=2,
-            amplifierOn=1,
             expMsgTxt="",
             expErrTxt="",
         )
